9bf745185de678c67c9418dc7086a1f036372ef2
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2021-10-01 21:16:57 +0000

    added invalid number check

--- cmpe1300/ICA03/ICA03/Program.cs
+++ cmpe1300/ICA03/ICA03/Program.cs
@@ -8,27 +8,33 @@ namespace ICA03
         {
             //Declare variables
             string title = "Nandish Patel - Assignment 3";
-            double r = 0;
+            double rad = 0;
             string type = "";
             double calc = 0;
 
             Console.WriteLine(title);
 
             Console.WriteLine("Enter the radius of a circle or sphere: ");
-            double.TryParse(Console.ReadLine(), out r);
+            double.TryParse(Console.ReadLine(), out rad);
+            if (rad<0)
+            {
+                Console.Write("\nThe number you have entered is invalid. Press any key to exit: ");
+                Console.ReadKey();
+                System.Environment.Exit(1);
+            }
 
             Console.Write("Please enter the desire calculation [A]rea or [V]olume: ");
             type = Console.ReadLine();
 
             if (type == "a"||type=="A")
             {
-                calc = Math.PI * r * r;
-                Console.WriteLine($"The area of a circle with a radius of {r} is {calc:F3}");
+                calc = Math.PI * rad * rad;
+                Console.WriteLine($"The area of a circle with a radius of {rad} is {calc:F3}");
             }
             else if (type == "v"||type=="V")
             {
-                calc = Math.PI * r * r * r * 4 / 3;
-                Console.WriteLine($"The volume of a sphere with a radius of {r} is {calc:F3}");
+                calc = Math.PI * rad * rad * rad * 4 / 3;
+                Console.WriteLine($"The volume of a sphere with a radius of {rad} is {calc:F3}");
             }
             else
             {

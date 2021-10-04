8ebc0a9a5a8558fe1eb16920069eafadbafd9cc6
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2021-10-04 02:31:08 +0000

    added some comments and adjusted invalid radius to included zero

--- cmpe1300/ICA03/ICA03/Program.cs
+++ cmpe1300/ICA03/ICA03/Program.cs
@@ -15,25 +15,25 @@ namespace ICA03
             Console.WriteLine(title);
 
             Console.WriteLine("Enter the radius of a circle or sphere: ");
-            double.TryParse(Console.ReadLine(), out rad);
-            if (rad<0)
+            double.TryParse(Console.ReadLine(), out rad); //parses input as double and assigns it to variable. input will be zero if not a valid 'double'
+            if (rad<=0) //checks if rad is less than or equal to zero. if so, exits program.
             {
-                Console.Write("\nThe number you have entered is invalid. Press any key to exit: ");
+                Console.Write("\nThe number you have entered is invalid. Expecting a real number greater than 0. Press any key to exit: ");
                 Console.ReadKey();
                 System.Environment.Exit(1);
             }
 
-            Console.Write("Please enter the desire calculation [A]rea or [V]olume: ");
+            Console.Write("Please enter the desire calculation [A]rea or [V]olume: "); //user selection
             type = Console.ReadLine();
 
-            if (type == "a"||type=="A")
+            if (type == "a"||type=="A") //checks which calculation the user wants and executes it.
             {
-                calc = Math.PI * rad * rad;
+                calc = Math.PI * rad * rad; //formula for area of circle
                 Console.WriteLine($"The area of a circle with a radius of {rad} is {calc:F3}");
             }
             else if (type == "v"||type=="V")
             {
-                calc = Math.PI * rad * rad * rad * 4 / 3;
+                calc = Math.PI * rad * rad * rad * 4 / 3; //formula for volume of sphere
                 Console.WriteLine($"The volume of a sphere with a radius of {rad} is {calc:F3}");
             }
             else

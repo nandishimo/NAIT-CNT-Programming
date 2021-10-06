f8712ac9eb2b94134d8ed6de56d3081df2c61843
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2021-10-06 20:26:38 +0000

    comments

--- cmpe1300/Exam 1 Q2/Exam 1 Q2/Program.cs
+++ cmpe1300/Exam 1 Q2/Exam 1 Q2/Program.cs
@@ -17,20 +17,23 @@ namespace Exam_1_Q2
                 *************************************/
 
             //Title and variables
+
+            //discount constant variables
             const double s = 0.05;
             const double q = 0.12;
             const double v = 0.17;
 
-            int nDrinks;
-            int packs;
-            int singles;
+            int nDrinks; //for user entry
+            int packs;//for calcs
+            int singles;//for calcs
 
+            //for calcs of cost of packs, singles, total, discount
             double cPacks;
             double cSingles;
             double cTotal;
-            double cDisc=0;
+            double cDisc=0;//default discount will be zero
 
-            string coupon;
+            string coupon;//for user coupon entry
             string title = "Exam 1, Question 2 - Nandish Patel";
             Console.SetCursorPosition(Console.WindowWidth / 2 - title.Length / 2, 0);
             Console.WriteLine(title);
@@ -38,7 +41,7 @@ namespace Exam_1_Q2
             //Ask user for # of energy drinks to purchase. Detect invalid inputs
             Console.Write("\nEnter the number of energy drinks to purchase: ");
             int.TryParse(Console.ReadLine(), out nDrinks);
-            if(nDrinks <=0)
+            if(nDrinks <=0)//invalid input handling for number of drinks
             {
                 Console.WriteLine("\nEnter a real number greater than zero. Press any key to exit . . . ");
                 Console.Read();
@@ -47,31 +50,30 @@ namespace Exam_1_Q2
 
             //Ask user for discount coupon. Invalid or no code means no discount.
             Console.Write("\nEnter your coupon code. Enter 'n' if no coupon: ");
-            coupon = Console.ReadLine();
+            coupon = Console.ReadLine();//assign user input to string 'coupon'
 
             //Calculate number of 8-packs and singles required.
-            packs = nDrinks / 8;
-            singles = nDrinks % 8;
+            packs = nDrinks / 8;//integer division for 8-packs
+            singles = nDrinks % 8;//remainder of division for singles
 
             //Calculate total cost and apply discount
-            cPacks = packs * 7.95;
-            cSingles = singles * 1.49;
-            cTotal = cPacks + cSingles;
-            if (coupon == "s") { cDisc = cTotal*s; }
+            cPacks = packs * 7.95; //calc cost of packs
+            cSingles = singles * 1.49; //calc cost of singles
+            cTotal = cPacks + cSingles; //calc total cost
+            if (coupon == "s") { cDisc = cTotal*s; }//determine which discount to apply based on the user input
             else if (coupon == "q") { cDisc = cTotal * q; }
             else if (coupon == "v") { cDisc = cTotal * v; }
-            cTotal = cTotal-cDisc;
+            cTotal = cTotal-cDisc;//calc total cost after discount
 
             //Display number of packs and singles to purchase, and the total cost
             Console.WriteLine($"\nTo buy {nDrinks} Energy Drinks, you need {packs} pack(s) and {singles} bottle(s).");
             Console.WriteLine($"\nThe total price is {cTotal:C} for the drinks.");
-            if(cDisc!=0)
+
+            //If a valid coupon was entered, display savings.
+            if (cDisc != 0)//check if non-zero discount was applied and display.
             {
                 Console.WriteLine($"\nYou saved {cDisc:C} with your coupon!");
             }
-
-
-            //If a valid coupon was entered, display savings.
 
             //End program
             Console.Write("\nPress the <ENTER> key to exit . . . ");

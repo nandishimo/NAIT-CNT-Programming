ef866946d229349829f078997053eaeae0bdea8e
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2021-10-06 19:34:13 +0000

    Fixed some formatting and added another validation

--- cmpe1300/Exam 1/Exam 1/Program.cs
+++ cmpe1300/Exam 1/Exam 1/Program.cs
@@ -23,20 +23,35 @@ namespace Exam_1
             double Vliq;
 
             //Ask user for outer and inner ball radii
+            //Validate inputs and exit program if invalid
             Console.Write("Input radius of outer ball (cm): ");
             double.TryParse(Console.ReadLine(), out Rout);
-            
-            Console.Write("\nInput radius of inner ball (cm): ");
-            double.TryParse(Console.ReadLine(), out Rin);
-            //Validate inputs and exit program if invalid
-            if (Rout<=0||Rin<=0)
+            if (Rout <= 0)
             {
-                Console.WriteLine("Please enter a real number greater than zero for the radius. Press any key to exit: ");
+                Console.Write("\nPlease enter a real number greater than zero for the radius. Press any key to exit: ");
                 Console.ReadKey();
                 System.Environment.Exit(1);
             }
+
+            Console.Write("\nInput radius of inner ball (cm): ");
+            double.TryParse(Console.ReadLine(), out Rin);
+            if (Rin <= 0)
+            {
+                Console.Write("\nPlease enter a real number greater than zero for the radius. Press any key to exit: ");
+                Console.ReadKey();
+                System.Environment.Exit(1);
+            }
+
+            if (Rout <= Rin)
+            {
+                Console.Write("\nRadius of outer ball must be greater than radius of inner ball! Press any key to exit: ");
+                Console.ReadKey();
+                System.Environment.Exit(1);
+            }
+
+
             //Display outer and inner radii 
-            Console.WriteLine("\n\n-------------------------------------------");
+            Console.WriteLine("\n\n-------------------------------------------\n");
             Console.WriteLine($"Radius of Outer Ball:\t{Rout:F2} cm");
             Console.WriteLine($"Radius of Inner Ball:\t{Rin:F2} cm");
 
@@ -44,11 +59,11 @@ namespace Exam_1
             Vout = Math.Pow(Rout, 3) * 4 / 3 * Math.PI;
             Vin = Math.Pow(Rin, 3) * 4 / 3 * Math.PI;
             Vliq = Vout - Vin;
-            Console.WriteLine($"Volume of Outer Ball:\t{Vout:F2} cm");
-            Console.WriteLine($"Volume of Inner Ball:\t{Vin:F2} cm");
-            Console.WriteLine($"Volume of Liquid:\t{Vliq:F2} cm");
+            Console.WriteLine($"Volume of Outer Ball:\t{Vout:F2} cubic cm");
+            Console.WriteLine($"Volume of Inner Ball:\t{Vin:F2} cubic cm");
+            Console.WriteLine($"Volume of Liquid:\t{Vliq:F2} cubic cm ");
             //End program 
-            Console.Write("\nPress any key to continue...");
+            Console.Write("\nPress any key to continue . . . ");
             Console.ReadKey();
 
 

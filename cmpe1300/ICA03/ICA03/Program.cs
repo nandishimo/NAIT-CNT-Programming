5cd04cddba5071aa496151ced7638b2d5ff7c088
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2021-10-06 18:08:10 +0000

    Read the ICA 3 instructions and realized I didnt follow all the steps. Added/changed a few things around

--- cmpe1300/ICA03/ICA03/Program.cs
+++ cmpe1300/ICA03/ICA03/Program.cs
@@ -12,40 +12,42 @@ namespace ICA03
             string type = "";
             double calc = 0;
 
+            Console.SetCursorPosition(Console.WindowWidth / 2 - title.Length / 2, 0);
             Console.WriteLine(title);
 
-            Console.WriteLine("Enter the radius of a circle or sphere: ");
+            Console.Write("\nEnter the radius of a circle or sphere: ");
             double.TryParse(Console.ReadLine(), out rad); //parses input as double and assigns it to variable. input will be zero if not a valid 'double'
             if (rad<=0) //checks if rad is less than or equal to zero. if so, exits program.
             {
-                Console.Write("\nThe number you have entered is invalid. Expecting a real number greater than 0. Press any key to exit: ");
+                Console.Write("\n\nThe number you have entered is invalid. Expecting a real number greater than 0. Press any key to exit: ");
                 Console.ReadKey();
                 System.Environment.Exit(1);
             }
 
-            Console.Write("Please enter the desire calculation [A]rea or [V]olume: "); //user selection
+            Console.Write("\n\nPlease enter the desire calculation [area] or [volume]: "); //user selection
             type = Console.ReadLine();
+            type = type.ToLower(); //convert to lower case
 
-            if (type == "a"||type=="A") //checks which calculation the user wants and executes it.
+            if (type == "area") //checks which calculation the user wants and executes it.
             {
                 calc = Math.PI * rad * rad; //formula for area of circle
-                Console.WriteLine($"The area of a circle with a radius of {rad} is {calc:F3}");
+                Console.WriteLine($"\nThe area of a circle with a radius of {rad} is {calc:F3}");
             }
-            else if (type == "v"||type=="V")
+            else if (type == "volume")
             {
                 calc = Math.PI * rad * rad * rad * 4 / 3; //formula for volume of sphere
-                Console.WriteLine($"The volume of a sphere with a radius of {rad} is {calc:F3}");
+                Console.WriteLine($"\nThe volume of a sphere with a radius of {rad} is {calc:F3}");
             }
-            else
+            else //invalid entry handling
             {
-                Console.WriteLine("You have not entered a valid calculation type. The program will now exit.");
+                Console.WriteLine("You have not entered a valid calculation type. Expecting either [area] or [volume]. The program will now exit.");
             }
 
 
             //End program
-            Console.Write("Press <enter> to exit: ");
+            Console.Write("\nPress any key to exit: ");
 
-            Console.Read();
+            Console.Readkey();
         }
     }
 }

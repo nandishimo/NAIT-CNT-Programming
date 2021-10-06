7feaec4ec887cc1c1d0b37082d70f7c1e356bf32
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2021-10-06 19:42:42 +0000

    Pseudo code

--- cmpe1300/Exam 1 Q2/Exam 1 Q2/Program.cs
+++ cmpe1300/Exam 1 Q2/Exam 1 Q2/Program.cs
@@ -16,7 +16,23 @@ namespace Exam_1_Q2
 
                 *************************************/
             
-            //
+            //Title and variables
+
+            //Ask user for # of energy drinks to purchase. Detect invalid inputs
+
+            //Ask user for discount coupon. Invalid or no code means no discount.
+
+            //Calculate number of 8-packs and singles required.
+
+            //Calculate total cost and apply discount
+
+            //Display number of packs and singles to purchase, and the total cost
+
+            //If a valid coupon was entered, display savings.
+
+            //end program
+
+
         }
     }
 }

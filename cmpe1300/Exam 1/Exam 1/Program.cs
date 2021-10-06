5f593e0fc5957d67bf9677ce29215bcbaf7cd915
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2021-10-06 19:09:14 +0000

    Pseudo code

--- cmpe1300/Exam 1/Exam 1/Program.cs
+++ cmpe1300/Exam 1/Exam 1/Program.cs
@@ -6,7 +6,25 @@ namespace Exam_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* *************************************
+
+                * NANDISH PATEL
+
+                * LAB EXAM 1 Q1
+
+                * Exam Code: 85,352
+
+                *************************************/
+            //Ask user for outer and inner ball radii
+
+            //Validate inputs and exit program if invalid
+
+            //Display outer and inner radii 
+
+            //calculate and display volume of outer and inner ball and the liquid (the difference)
+
+            //End program 
+
         }
     }
 }

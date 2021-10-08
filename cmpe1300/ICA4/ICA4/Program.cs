d190bb0459ca1d5d0de60ed3fc54b21fbec4eb43
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2021-10-08 20:59:35 +0000

    some pseudo code and starting to code

--- cmpe1300/ICA4/ICA4/Program.cs
+++ cmpe1300/ICA4/ICA4/Program.cs
@@ -6,7 +6,29 @@ namespace ICA4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declare variables
+            string userChoice;
+            string C;
+            string computerChoice;
+            int rand;
+            Random randomNumber = new Random();
+            string[] RPS = { "rock", "paper", "scissors" };
+
+            //display title
+
+            //diplay RPS choices and ask for user input
+            Console.WriteLine("Please select your play from the following options:\n\n Paper\n Rock\n Scissors");
+            Console.Write("\nYour selection is: ");
+            userChoice = Console.ReadLine().ToLower();
+
+            //use RNG to determine computer's choice
+            computerChoice=RPS[randomNumber.Next(0,2)];
+
+            //display outcome
+            Console.WriteLine($"\nYou played {userChoice}. The computer played {computerChoice}");
+
+            //end program
+            Console.Read();
         }
     }
 }

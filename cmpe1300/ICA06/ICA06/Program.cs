eb889a38875f158560afcd1c339cc1e42485dfc9
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2021-10-20 20:16:58 +0000

    psuedo code

--- cmpe1300/ICA06/ICA06/Program.cs
+++ cmpe1300/ICA06/ICA06/Program.cs
@@ -6,13 +6,28 @@ using System.Threading.Tasks;
 using System.Drawing;
 using GDIDrawer;
 
-
 namespace ICA06
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //declare variables
+
+            //Display title
+            string title = "Nandish Patel - ICA06";
+            Console.CursorLeft = Console.WindowWidth / 2 - title.Length / 2;
+            Console.WriteLine(title);
+
+            //Create GDIDrawer window of size 800x600
+
+            //query square size from user from 10 to 200. validate as int. use a while loop with error message if invalid
+
+            //query number of squares to draw as integer. check if valid and greater than zero. use a while loop with error message if invalid
+
+            //draw squares of specified size and number in random colors at random locations in the GDIDrawer window
+
+            //repeat program in a do-while loop if user enters 'y' or 'Y'. clear the console andd GDIDrawer window
 
         }
     }

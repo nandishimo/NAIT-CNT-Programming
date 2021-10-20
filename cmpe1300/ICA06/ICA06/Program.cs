37662436571d0db6c6804cd1b0b2c08d5b8223d5
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2021-10-20 20:51:30 +0000

    basic loop, without the drawing output

--- cmpe1300/ICA06/ICA06/Program.cs
+++ cmpe1300/ICA06/ICA06/Program.cs
@@ -13,21 +13,42 @@ namespace ICA06
         static void Main(string[] args)
         {
             //declare variables
+            int size;
+            int number;
+            char cont;
 
-            //Display title
-            string title = "Nandish Patel - ICA06";
-            Console.CursorLeft = Console.WindowWidth / 2 - title.Length / 2;
-            Console.WriteLine(title);
+            //main do-while loop
+            do
+            {
+                //Display title
+                string title = "Nandish Patel - ICA06";
+                Console.CursorLeft = Console.WindowWidth / 2 - title.Length / 2;
+                Console.WriteLine(title);
 
-            //Create GDIDrawer window of size 800x600
+                //Create GDIDrawer window of size 800x600
+                CDrawer window = new CDrawer(800, 600);
 
-            //query square size from user from 10 to 200. validate as int. use a while loop with error message if invalid
+                //query square size from user from 10 to 200. validate as int. use a while loop with error message if invalid
+                Console.Write("\nPlease enter an integer from 10 to 200 for the square size: ");
+                while (!int.TryParse(Console.ReadLine(), out size) || size < 10 || size > 200)
+                {
+                    Console.Write("\nYou must enter a valid integer between 10 and 200: ");
+                }
 
-            //query number of squares to draw as integer. check if valid and greater than zero. use a while loop with error message if invalid
+                //query number of squares to draw as integer. check if valid and greater than zero. use a while loop with error message if invalid
+                Console.Write("\nPlease enter a positive integer for number of squares you would like drawn: ");
+                while (!int.TryParse(Console.ReadLine(), out number) || number < 1)
+                {
+                    Console.Write("\nYou must enter a valid integer greater than 0: ");
+                }
+                //draw squares of specified size and number in random colors at random locations in the GDIDrawer window
+                //window.AddRectangle(size, size, RandColor.GetColor());
 
-            //draw squares of specified size and number in random colors at random locations in the GDIDrawer window
-
-            //repeat program in a do-while loop if user enters 'y' or 'Y'. clear the console andd GDIDrawer window
+                //repeat program in a do-while loop if user enters 'y' or 'Y'. clear the console andd GDIDrawer window
+                Console.Write("Would you like to run the program again? 'Y' or 'N'");
+                char.TryParse(Console.ReadLine(), out cont);
+            } while (cont == 'y' || cont == 'Y');
+            Console.Read();
 
         }
     }

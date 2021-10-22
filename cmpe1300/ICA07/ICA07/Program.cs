abc93c4a70b9a1585cedbbeaee8aca670e0270d1
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2021-10-22 20:47:05 +0000

    Adding to subversion. with pseudo code and some code started

---
+++ cmpe1300/ICA07/ICA07/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICA07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare Variables
+            string title = "Nandish Patel - ICA07";
+            int nGrades;
+            Random randGrade = new Random();
+
+            do
+            {
+                //Display a title with name and assignment number
+                Console.Clear();
+                Console.CursorLeft = Console.WindowWidth / 2 - title.Length / 2;
+                Console.WriteLine(title);
+
+                //Input number of grades to generate as an int. Check that the value is valid int from 5 to 10.
+                //If invalid,display an error message and use a do -while loop to perform the input operation again. Display different error messages if the value is invalid, too low or too high.
+                Console.Write("\nEnter number of grades to generate (from 5 to 10): ");
+                int.TryParse(Console.ReadLine(), out nGrades);
+
+
+
+                //Using a do -while loop, generate the number of double grades in the range of 0.0 to 100.0 and display each grade using one decimal place as shown below.
+                do
+                {
+
+                } while ();
+
+
+                //Display the average of the grades with one decimal place of accuracy.
+
+
+                //Display the number of failing grades.
+
+
+                //Using a do -while loop, repeat the program if the user enters ‘y’ or ‘Y’. Clear the Console window.
+
+
+            }
+        }
+    }
+}

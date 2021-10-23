1f60b47fcf27d472245ea4674d98c88d04ae7539
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2021-10-23 03:30:04 +0000

    project start. pseudo code in

---
+++ cmpe1300/ICA08/ICA08/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICA08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            string title = "Nandish Patel - ICA08";
+            int i;
+            int size;
+            char cont = 'y';
+
+            for (i=0; i<100; i++) 
+            {
+                //display title
+
+                //input the shape size as integer. check for valid input from 5 to 25.
+
+                //if invalid use loop to display error messages and get new input
+
+                //input the type of shape as "line" "square" or "triangle". use loop to force user to enter a correct shape
+
+                //draw the shape using for loops with the '*' character
+
+                //user to run program again
+            }
+
+
+        }
+    }
+}

f959401505ceb3a9eb3c1dfc9f4ead2176a3dc46
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2021-11-09 18:22:56 +0000

    Added border and score counter on screen

--- cmpe1300/Lab 2/Lab 2/Program.cs
+++ cmpe1300/Lab 2/Lab 2/Program.cs
@@ -14,10 +14,15 @@ namespace Lab_2
         static void Main(string[] args)
         {
             //declare variable
+            //balls starts on left side and moves right
             int xBall = 2;
             int yBall = 59;
             int xVel = 1;
             int yVel = 1;
+            int score = 0;
+            int bbX;
+            int bbY;
+
             Point mPos;
 
             //use gdidrawer with continuous update mode off and scale of 5. windows size of 160x120
@@ -26,18 +31,40 @@ namespace Lab_2
             CDrawer game = new CDrawer(800, 600, false);
             game.Scale = 5;
 
+            for(bbX=0; bbX<160; bbX++)
+            {
+                game.SetBBScaledPixel(bbX, 0, Color.Aqua);
+                game.SetBBScaledPixel(bbX, 119, Color.Aqua);
+            }
+            for (bbY = 0; bbY < 120; bbY++)
+            {
+                game.SetBBScaledPixel(159, bbY, Color.Aqua);
+            }
+
+            //loop to animate ball and move the paddle with time delay of 20ms
+            //game ends when ball exits left side of game board
             while (xBall > 0)
             {
+                //y position of mouse will determine paddle position, read every animation loop to move the paddle
                 game.GetLastMousePositionScaled(out mPos);
                 game.AddLine(0, mPos.Y-5, 0, mPos.Y + 5, Color.Blue, 10);
+                //create ball with position x,y and velocty xVel and yVel
                 game.AddRectangle(xBall, yBall, 2, 2, Color.Red);
                 game.Render();
-                System.Threading.Thread.Sleep(10);
+                System.Threading.Thread.Sleep(10); //time delay of 10ms
                 game.Clear();
+                game.AddText($"Score: {score}", 100, Color.DarkSlateGray);
+                //ball bounces off the paddle and walls in opposite directionby 90deg
                 if (xBall > 158)
                     xVel = -xVel;
+                
                 if (xBall < 2 && yBall < mPos.Y + 6 && yBall > mPos.Y - 8)
+                {
                     xVel = -xVel;
+                    //paddle bounces increase score by 1
+                    score++;
+                }
+                    
                 if (yBall > 118 || yBall < 1)
                     yVel = -yVel;
 
@@ -45,17 +72,7 @@ namespace Lab_2
                 yBall += yVel;//update ball position
             }
 
-            //loop to animate ball and move the paddle with time delay of 20ms
-            //create ball with position x,y and velocty xVel and yVel
-
-            //ball bounces off the paddle and walls in opposite directionby 90deg
-            //paddle bounces increase score by 1
-
-            //y position of mouse will determine paddle position, read every animation loop to move the paddle
-
-            //balls starts on left side and moves right
-
-            //game ends when ball exits left side of game board
+            
 
             //display two buttons "play again" and "quit". buttons to be clicked
 

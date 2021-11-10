a3f08f6601aa05f8e30c0967e389a6a873cef87e
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2021-11-10 20:54:06 +0000

    pseudo code and title

--- cmpe1300/Exam2_Q2/Exam2_Q2/Program.cs
+++ cmpe1300/Exam2_Q2/Exam2_Q2/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/* *************************************
+
+   * NANDISH PATEL
+
+   * LAB EXAM 2 Q2
+
+   * Question Code: 342
+
+   *************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +21,28 @@ namespace Exam2_Q2
     {
         static void Main(string[] args)
         {
+            //declare variables
+            string title = "Nandish Patel Exam 2 Q2";
+            string shape;
+
+            //display title
+            Console.CursorLeft = Console.WindowWidth / 2 - title.Length / 2;
+            Console.WriteLine(title);
+
+            //ask user to input circle or square and validate
+         
+
+
+
+            //loop for validation
+
+            //ask user for number of shapes n to be drawn
+            //loop for valid integer between 5 and 10
+
+            //create gdidrawer window 800x600, scale of 5
+
+            //draw n shapes of specified type and size 10x10, color=seagreen
+
         }
     }
 }

259db0683255376ce83e28b233fdc453e04f5ad2
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2021-11-10 01:11:22 +0000

    Basic loop

--- cmpe1300/ICA09/ICA09/Program.cs
+++ cmpe1300/ICA09/ICA09/Program.cs
@@ -11,18 +11,76 @@ namespace ICA09
         static void Main(string[] args)
         {
             //declare variables
+            string pass;
+            string title = "ICA09 - Nandish Patel";
+            int i = 0;
+            bool valid = false;
+            string rerun;
 
-            //display title
 
-            //user inputs a password
+            do
+            {
+                Console.Clear();
+                //display title
+                Console.CursorLeft = Console.WindowWidth / 2 - title.Length / 2;
+                Console.WriteLine(title);
 
-            //check password against specifications using foreach loops. display error message for each condition that has not been met
+                do
+                {
+                    valid = true;
+                    //user inputs a password
+                    Console.Write("Please enter a password: ");
+                    pass = Console.ReadLine();
 
-            //display message if password is acceptable
+                    //check password against specifications using foreach loops. display error message for each condition that has not been met
+                    //minimum 8 characters
+                    foreach (char ch in pass)
+                    {
+                        i++;
+                    }
+                    if (i < 7)
+                    {
+                        Console.WriteLine("Password must be minimum 8 characters in length.");
+                        valid = false;
+                    }
 
-            //loop program as long as user desires
+                    //one uppercase character
+                    i = 0;
+                    foreach (char ch in pass)
+                    {
+                        if (char.IsUpper(ch))
+                            i++;
+                    }
+                    if (i == 0)
+                    {
+                        Console.WriteLine("Password must contain at least one uppercase letter.");
+                        valid = false; ;
+                    }
 
-            Console.Read();
+                    //one lowercase character
+                    i = 0;
+                    foreach (char ch in pass)
+                    {
+                        if (char.IsLower(ch))
+                            i++;
+                    }
+                    if (i == 0)
+                        Console.WriteLine("Password must contain at least one lowercase letter.");
+                    //one symbol character
+                    //no space or tab characters
+                    //at least two different numbers
+
+                    //display message if password is acceptable
+                } while (!valid);
+
+                //loop program as long as user desires
+                Console.Write("Run program again? (Y/N): ");
+                rerun = Console.ReadLine();
+
+            } while (rerun == "Y" || rerun == "y");
+
+            Console.WriteLine("\nThe program will now exit...");
+            Console.ReadKey();
 
         }
     }

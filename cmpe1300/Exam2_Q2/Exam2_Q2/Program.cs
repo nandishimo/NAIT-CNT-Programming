cce0aff32f7bd8ffb35d18852e8cf7aded20cdb6
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2021-11-10 21:28:10 +0000

    comments

--- cmpe1300/Exam2_Q2/Exam2_Q2/Program.cs
+++ cmpe1300/Exam2_Q2/Exam2_Q2/Program.cs
@@ -23,12 +23,12 @@ namespace Exam2_Q2
         {
             //declare variables
             string title = "Nandish Patel Exam 2 Q2";
-            string shape;
-            int number=0;
-            bool valid;
-            const int size = 10;
-            Random position = new Random();
-            int i;
+            string shape;//stores shape type
+            int number=0;//stores # of shapes to draw
+            bool valid;//for valid check of shape
+            const int size = 10;//size of circle or square
+            Random position = new Random();//generates random numbers for shape location
+            int i;//counter
 
             //display title
             Console.CursorLeft = Console.WindowWidth / 2 - title.Length / 2;
@@ -40,23 +40,23 @@ namespace Exam2_Q2
             {
                 Console.Write("Input Shape (circle or square): ");
                 shape = Console.ReadLine();
-                shape = shape.ToLower();
+                shape = shape.ToLower();//convert input to lowercase. allows combination of capital and lowercase characters to be accepted
                 if (shape != "circle" && shape != "square")
-                    Console.WriteLine($"\nWrong Shape- Please Input again.\n");
-            } while (shape != "circle" && shape != "square");
+                    Console.WriteLine($"\nWrong Shape- Please Input again.\n");//error message
+            } while (shape != "circle" && shape != "square");//stuck in loop until cirle or square is entered
 
             //ask user for number of shapes n to be drawn
             //loop for valid integer between 5 and 10
             do
             {
                 Console.Write($"\nInput the number of {shape}s to be drawn (5-10): ");
-                valid = int.TryParse(Console.ReadLine(), out number);
+                valid = int.TryParse(Console.ReadLine(), out number);//check input if integer
                 if (!valid)
                 {
-                    Console.Write("\nValue invalid- -You should enter an integer value\n");
+                    Console.Write("\nValue invalid- -You should enter an integer value\n");//error message
                 }
 
-            } while (!valid);
+            } while (!valid);//stuck in loop until integer is entered
             //set number to 5 if less than 5, set to 10 if greater than 10
             number = (number < 5) ? 5 : number;
             number = (number > 10) ? 10 : number;
@@ -66,18 +66,20 @@ namespace Exam2_Q2
             window.Scale = 5;
 
             //draw n shapes of specified type and size 10x10, color=seagreen
-            if (shape == "circle")
+            if (shape == "circle")//if circle draw cricle
             {
                 for (i = 0; i < number; i++)
                 {
+                    //draw circle at random positions 
                     window.AddEllipse(position.Next(0, 150), position.Next(0, 110), size, size, Color.SeaGreen);
                 }
                 
             }
-            if (shape == "square")
+            if (shape == "square")//if square draw square
             {
                 for (i = 0; i < number; i++)
                 {
+                    //draw square at random positions
                     window.AddRectangle(position.Next(0, 150), position.Next(0, 110), size, size, Color.SeaGreen);
                 }
 

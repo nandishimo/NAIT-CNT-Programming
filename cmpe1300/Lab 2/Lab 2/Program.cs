9b80fc1336f42edfcf7a72a166821bd77f8ee9e5
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2021-11-10 07:13:19 +0000

    added play again and quit buttons. seems to be functioning. has to adjust how the loops worked

--- cmpe1300/Lab 2/Lab 2/Program.cs
+++ cmpe1300/Lab 2/Lab 2/Program.cs
@@ -13,26 +13,24 @@ namespace Lab_2
     {
         static void Main(string[] args)
         {
-            //declare variable
-            //balls starts on left side and moves right
-            int xBall = 2;
-            int yBall = 59;
-            int xVel = 1;
-            int yVel = 1;
-            int score = 0;
+            bool again = true;
+            int xBall;
+            int yBall;
+            int xVel;
+            int yVel;
+            int score;
             int bbX;
             int bbY;
             Point clickPos;
-
             Point mPos;
+            bool selection=false;
 
             //use gdidrawer with continuous update mode off and scale of 5. windows size of 160x120
-            //ball as rectangle with size 2, paddle is line with thickness 10
-            //three table edgesdrawn on background
             CDrawer game = new CDrawer(800, 600, false);
             game.Scale = 5;
+            //three table edges drawn on background
 
-            for(bbX=0; bbX<160; bbX++)
+            for (bbX = 0; bbX < 160; bbX++)
             {
                 game.SetBBScaledPixel(bbX, 0, Color.Aqua);
                 game.SetBBScaledPixel(bbX, 119, Color.Aqua);
@@ -41,51 +39,85 @@ namespace Lab_2
             {
                 game.SetBBScaledPixel(159, bbY, Color.Aqua);
             }
-
-            //loop to animate ball and move the paddle with time delay of 20ms
-            //game ends when ball exits left side of game board
-            while (xBall > 0)
+            do
             {
-                //y position of mouse will determine paddle position, read every animation loop to move the paddle
-                game.Clear();
-                game.GetLastMousePositionScaled(out mPos);
-                game.AddLine(0, mPos.Y-5, 0, mPos.Y + 5, Color.Blue, 10);
-                //create ball with position x,y and velocty xVel and yVel
-                game.AddRectangle(xBall, yBall, 2, 2, Color.Red);
-                game.AddText($"Score: {score}", 100, Color.DarkSlateGray);
-                game.Render();
-                //System.Threading.Thread.Sleep(10); //time delay of 10ms
+
+
+                //declare variable
+                //balls starts on left side and moves right
+                xBall = 2;
+                yBall = 59;
+                xVel = 1;
+                yVel = 1;
+                score = 0;
                 
-                //ball bounces off the paddle and walls in opposite directionby 90deg
-                if (xBall > 158)
-                    xVel = -xVel;
-                
-                if (xBall < 2 && yBall < mPos.Y + 6 && yBall > mPos.Y - 8)
+                //loop to animate ball and move the paddle with time delay of 20ms
+                //game ends when ball exits left side of game board
+                while (xBall > 0)
                 {
-                    xVel = -xVel;
-                    //paddle bounces increase score by 1
-                    score++;
-                }
+                    System.Threading.Thread.Sleep(10); //time delay
+                    game.Clear();
+                    //ball as rectangle with size 2, paddle is line with thickness 10
+                    //y position of mouse will determine paddle position, read every animation loop to move the paddle
+                    game.GetLastMousePositionScaled(out mPos);
+                    game.AddLine(0, mPos.Y - 5, 0, mPos.Y + 5, Color.Blue, 10);
+                    //create ball with position x,y and velocty xVel and yVel
+                    game.AddRectangle(xBall, yBall, 2, 2, Color.Red);
+                    game.AddText($"Score: {score}", 100, Color.DarkSlateGray);
+                    game.Render();
                     
-                if (yBall > 118 || yBall < 1)
-                    yVel = -yVel;
 
-                xBall += xVel;//update ball position
-                yBall += yVel;//update ball position
-            }
+                    //ball bounces off the paddle and walls in opposite directionby 90deg
+                    if (xBall > 158)
+                        xVel = -xVel;
+
+                    if (xBall < 2 && yBall < mPos.Y + 6 && yBall > mPos.Y - 8)
+                    {
+                        xVel = -xVel;
+                        //paddle bounces increase score by 1
+                        score++;
+                    }
+
+                    if (yBall > 118 || yBall < 1)
+                        yVel = -yVel;
+
+                    xBall += xVel;//update ball position
+                    yBall += yVel;//update ball position
+                }
 
 
-            //display two buttons "play again" and "quit" buttons to be clicked
-            game.AddRectangle(50, 100, 20, 10, Color.Green, 1,Color.Green);
-            game.AddText("Play Again", 10, 50, 100, 20, 10, Color.White);
-            game.AddRectangle(90, 100, 20, 10);
-            game.AddText("Quit", 10, 90, 100, 20, 10, Color.White);
-            game.Render();
-            game.GetLastMouseLeftClickScaled(out clickPos);
-            //if (clickPos.X<70 && clickPos.X>50  
+                //display two buttons "play again" and "quit" buttons to be clicked
+                game.AddRectangle(50, 100, 20, 10, Color.Green, 1, Color.Green);
+                game.AddText("Play Again", 10, 50, 100, 20, 10, Color.White);
+                game.AddRectangle(90, 100, 20, 10);
+                game.AddText("Quit", 10, 90, 100, 20, 10, Color.White);
+                game.Render();
+                selection = false;
 
+                while (!selection)
+                {
+                    clickPos = new Point(0,0);
+                    if(game.GetLastMouseLeftClickScaled(out clickPos))
+                    {
+                        if (clickPos.Y > 99 && clickPos.Y < 111)
+                        {
+                            if (clickPos.X > 49 && clickPos.X < 71)
+                            {
+                                selection = true;
+                                again = true;
+                            }
+                            if (clickPos.X > 89 && clickPos.X < 101)
+                            {
+                                selection = true;
+                                again = false;
+                            }
+                        }
+                    }
+                }
 
-            Console.Read();
+            } while (again);
+
+            game.Close();
         }
     }
 }

68c2229878ff801c256f39460a7e38147a4f11a5
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2021-11-10 00:26:11 +0000

    add to subversion. some pseudocode

---
+++ cmpe1300/ICA09/ICA09/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICA09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+
+            //display title
+
+            //user inputs a password
+
+            //check password against specifications using foreach loops. display error message for each condition that has not been met
+
+            //display message if password is acceptable
+
+            //loop program as long as user desires
+
+            Console.Read();
+
+        }
+    }
+}

ff83b46de6edae92f54737c0846dcb02a0787922
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2021-11-10 20:19:48 +0000

    pseudo code

--- cmpe1300/Exam_2_Q1/Exam_2_Q1/Program.cs
+++ cmpe1300/Exam_2_Q1/Exam_2_Q1/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/* *************************************
+
+   * NANDISH PATEL
+
+   * LAB EXAM 2 Q1
+
+   * Question Code: 83,678
+
+   *************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +21,32 @@ namespace Exam_2_Q1
     {
         static void Main(string[] args)
         {
+            //declare variables
+            string title = "Nandish Patel Exam 2 Numbers";
+            int runs;
+
+            //display title
+            Console.CursorLeft = Console.WindowWidth / 2 - title.Length / 2;
+            Console.WriteLine(title);
+
+            //ask user how many times to run program. must be a valid number between 3 and 9
+            //do-while input validation loop
+
+            
+
+            //while program loop
+            //ask user for a sentence and display the sentance number starting at 1
+            //while sentence validation loop
+            //sentence must include a number to be valid
+            //user can enter *(constant) to use the default sentence 'I like to eat 3 pies'
+            //each number n found in the sentence must be displayed n number of times
+            //foreach loop
+            //each digit character stored as int and displayed that many times
+
+            //exit program after specified runs and upon pressing a key
+
+
+
         }
     }
 }

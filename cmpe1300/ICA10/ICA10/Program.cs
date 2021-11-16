fadb611026f89fa6f1622bb23bbaf186166cc576
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2021-11-16 18:32:29 +0000

    pseudo

--- cmpe1300/ICA10/ICA10/Program.cs
+++ cmpe1300/ICA10/ICA10/Program.cs
@@ -10,8 +10,41 @@ namespace ICA10
 {
     class Program
     {
+        //main method
+        //input line length, angle increment as doubles
+        //input spacing as int
+        //run DrawStars() method in a loop
         static void Main(string[] args)
-        {
+        {  
+            
         }
+
+        //GetInt() method iwth 3 parameters (input, minimum value, maximum value
+        static private void GetInt()
+        {
+
+        }
+        //prompt user for a value to return to calling code
+        //force user to input a valid integer within range
+        //use loop
+
+        //GetDouble() method with 3 parameters (input, min value, max value)
+        static private void GetDouble()
+        {
+
+        }
+        //prompt user for a value to return to calling code
+        //force user to input a valid double within range
+        //use loop
+
+        //Deg2Rad() method
+
+        //passed a double value from main
+        //performs calculation and returns angle in radians to main
+
+        //DrawStars method
+        //pass from main: GDIDrawer window as CDrawer, center point x-axis and y-axis for star,
+        //double line length for lines in star, double increment value in degrees
     }
+
 }

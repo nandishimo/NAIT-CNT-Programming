170738e23c6b68f1f06d2f1e380d67c40ee0d5c8
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2021-12-07 18:41:54 +0000

    comments

--- cmpe1300/ICA13/ICA13/Program.cs
+++ cmpe1300/ICA13/ICA13/Program.cs
@@ -12,23 +12,29 @@ namespace ICA13
         static void Main(string[] args)
         {
             string title = "Nandish Patel - ICA13";//title variable
+            bool again = false;
 
-            Console.CursorLeft = Console.WindowWidth / 2 - title.Length / 2;
-            Console.WriteLine(title);
+            do
+            {
+                Console.Clear();
+                Console.CursorLeft = Console.WindowWidth / 2 - title.Length / 2;
+                Console.WriteLine(title);//clear console and write centered title
 
-            int size = GetValue("Enter the size of the array: ", 4, 10);
-            int[] array = MakeArray(size);
+                int size = GetValue("Enter the size of the array: ", 4, 10);//get size between 4 and 10
+                int[] array = MakeArray(size); //create array of desired size
 
-            Show(array);
-            ShowReverse(array);
+                Show(array); //list array contents
+                ShowReverse(array); //llist array contents in reverse
 
-            Console.WriteLine($"The avaerage is {Average(array)}");
-            Console.WriteLine($"The largest value is {Largest(array)}");
+                Console.WriteLine($"\nThe average is {Average(array, size)}");//dispaly average array value
+                Largest(array, size, out double largest, out int location);
+                Console.WriteLine($"\nThe largest value is {largest} at location {location}");//display largest array valur and location
 
-            Console.Read();
+                again = RunAgain();//check to run program again
+            } while (again);
         }
 
-        static private int GetValue(string request, int min, int max)
+        static private int GetValue(string request, int min, int max)//get value method returns ints
         {
             bool valid = false;
             int output = 0;
@@ -45,12 +51,12 @@ namespace ICA13
             return output;
         }
 
-        static private int[] MakeArray(int size)
+        static private int[] MakeArray(int size)//make array method
         {
             Random random = new Random();
             int[] array;
             array = new int[size];
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < size; i++)//fills array with random ints
             {
                 array[i] = random.Next(0, 100);
             }
@@ -58,31 +64,55 @@ namespace ICA13
             return array;
         }
 
-        static private void Show(int[] array)
+        static private void Show(int[] array)//show array method
         {
             Console.WriteLine("\nThe array contents...");
             for(int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine($"array[{i}] = "+array[i]);
+                Console.WriteLine($"array[{i}] = "+array[i]);//loop through array indices and display contents
             }
 
         }
-        static private void ShowReverse(int[] array)
+        static private void ShowReverse(int[] array)//show reverse method
         {
-            Console.WriteLine("\nThe array in reverse...");
+            Console.WriteLine("\nThe array in reverse...");//loop through array in reverse and display contents
             for (int i = array.Length; i > 0; i--)
             {
                 Console.WriteLine($"array[{i}] = " + array[i-1]);
             }
 
         }
-        static private double Average(int[] array)
+        static private double Average(int[] array, int size)//average array method
         {
-            return Average(array);
+            double total = 0;
+            for (int i = 0; i < size; i++)//llop through array indices and add all values
+            {
+                total+=array[i];
+            }
+            return (total/size);//return total of array values divided by size for average
         }
-        static private double Largest(int[] array)
+        static private void Largest(int[] array, int size, out double largest, out int location)//largest array method, pass array and size from main
         {
-            return Largest(array);
+            largest = array[0];
+            location = 0;
+            for(int i=0; i<size; i++)
+            {
+                if (array[i] > largest)//loops through array and check if larger than current "largest" value
+                {
+                    largest = array[i];//replace largest value in this variable with current array value if the latter is larger
+                    location = i;//save location if array value is larger
+                }
+            }
+        }
+        static private bool RunAgain()
+        {
+            bool again=false;
+            Console.WriteLine("Would you like to run the program again? Yes or No");//prompt
+            string input = Console.ReadLine();
+            if (input == "Yes" || input == "yes")// if input is yes ot Yes, again is true, or else, its false and program exits
+                again = true;
+            return again;
+
         }
 
 

443b9693ebda9931bbbc5b493783448cb934e366
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-01-07 23:26:32 +0000

    comments added

--- cmpe1666/ICA01/ICA01/Program.cs
+++ cmpe1666/ICA01/ICA01/Program.cs
@@ -10,24 +10,30 @@ namespace ICA01
     {
         static void Main(string[] args)
         {
-            //display title
+            //declare variables
             string title = "CMPE 1666 - ICA01 - Winter 2022 - Nandish Patel";
+            int size;
+            int upper;
+            int lower;
+            int search;
+
+            //display title
             Console.CursorLeft = Console.WindowWidth / 2 - title.Length / 2;
             Console.WriteLine(title);
-            GetValue(out int size, "Please enter the number of values to be generated", 10, 100);
-            GetRange(out int upper, out int lower, 100, 0);
-            int[] array = GenerateArray(size, lower, upper);
-            ShowArray(array);
-            do
+            GetValue(out size, "Please enter the number of values to be generated", 10, 100);//prompt user for size and set limit of 10-100
+            GetRange(out upper, out lower, 100, 0); //prompt user for range of values to be generated with 0 andd 100 as bounds
+            int[] array = GenerateArray(size, lower, upper); //Generate Array using previously prompted variables
+            ShowArray(array); //display array
+            do //loop to search for occurences of values in array
             {
-                GetValue(out int search, "\nPlease enter a value to search for: ", lower, upper);
-                if (1 == CountOccurences(array, search))
+                GetValue(out search, "\nPlease enter a value to search for: ", lower, upper);//prompt user for value to search for
+                if (1 == CountOccurences(array, search))//checks if 1 occurence is found and displayes grammatically appropriate message
                     Console.WriteLine($"\nThere is 1 instance of {search} in the array.\n");
                 else
-                    Console.WriteLine($"\nThere are {CountOccurences(array, search)} instances of {search} in the array.\n");
+                    Console.WriteLine($"\nThere are {CountOccurences(array, search)} instances of {search} in the array.\n");//displays number of occurences found in the array
 
-            } while (GetAnswer("Would you like to search again? (y/n): "));
-            Console.Write("\nPress any key to exit...");
+            } while (GetAnswer("Would you like to search again? (y/n): "));//loops as long as user wants to search for more occurences
+            Console.Write("\nPress any key to exit...");//exit message
             Console.ReadKey();
         }
 
@@ -36,13 +42,13 @@ namespace ICA01
             bool valid = false;
             do
             {
-                Console.Write(request);
+                Console.Write(request);//display prompt
                 char answer = Console.ReadKey().KeyChar;
                 if (answer == 'y' || answer == 'Y')
-                    return true;
+                    return true;//y for positive return
                 if (answer == 'n' || answer == 'N')
-                    return false;
-                Console.WriteLine("\nThat is not a valid answer.");
+                    return false;//n for negative return
+                Console.WriteLine("\nThat is not a valid answer.");//if y or n were not pressed, error message displayed and user is looped
             } while (!valid);
             return false;
         }
@@ -53,7 +59,7 @@ namespace ICA01
             bool valid = false;
             do
             {
-                Console.Write(request+$" ({min}, {max}): ");
+                Console.Write(request+$" ({min}, {max}): ");//displays request with acceptable range.
                 string input = Console.ReadLine();
                 valid = int.TryParse(input, out value);//validate user input 
                 if (!valid)
@@ -76,7 +82,7 @@ namespace ICA01
                 do//lower range validation loop
                 {
                     GetValue(out lower, "Please enter a lower range: ", 0, 100);
-                    if (lower < lBound)//limit of -40 for range
+                    if (lower < lBound)//check lower limit
                     {
                         Console.WriteLine("Values this low cannot be displayed.");
                         valid2 = false;
@@ -88,7 +94,7 @@ namespace ICA01
                 do//upper range validation loop
                 {
                     GetValue(out upper, "Please enter an upper range: ", 0, 100);
-                    if (upper > uBound)//limit of 40 for range
+                    if (upper > uBound)//check upper limit
                     {
                         Console.WriteLine("Values this high cannot be displayed.");
                         valid3 = false;
@@ -96,7 +102,7 @@ namespace ICA01
                     else
                         valid3 = true;
                 } while (!valid3);
-                if (lower > upper)//lower less than upper validation loop
+                if (lower > upper)//checks if lower value is less than upper value
                 {
                     Console.WriteLine("Lower must be less than Upper range.");
                     valid1 = false;
@@ -111,11 +117,11 @@ namespace ICA01
 
         static private int[] GenerateArray(int size, int lower, int upper)
         {
-            int[] array = new int[size];
-            Random rand = new Random();
+            int[] array = new int[size];//generate array of defined size
+            Random rand = new Random();//create random variable
             for (int i = 0; i < size; i++)
             {
-                array[i] = rand.Next(lower, upper + 1);
+                array[i] = rand.Next(lower, upper + 1);//fill array with random numbers within upper and lower bounds
             }
             return array;
         }
@@ -126,13 +132,13 @@ namespace ICA01
             {
                 Console.Write(array[i]+", ");//loop through array indices and display contents
             }
-            Console.WriteLine(array[array.Length-1]);
+            Console.WriteLine(array[array.Length-1]);//display last element without comma
         }
 
         static private int CountOccurences(int[] array, int value)
         {
             int i = 0;
-            foreach(int num in array)
+            foreach(int num in array)//simple iteration through array with counter
             {
                 if (num == value)
                     i++;

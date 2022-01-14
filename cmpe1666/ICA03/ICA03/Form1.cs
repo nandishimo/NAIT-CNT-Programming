355f14de44b48054d6312c7ec0f999b99dce1f65
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-01-14 00:22:04 +0000

    Forgot initial commit. but this ICA is simple, so version control wouldnt show many instances of changes

---
+++ cmpe1666/ICA03/ICA03/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICA03
+{
+    public partial class Form1 : Form
+    {
+        public Form1() //constructor for class Form1
+        {
+            InitializeComponent();
+            System.Diagnostics.Trace.WriteLine($"{DateTime.Now} Constructor");
+            //write message to output on event
+        }
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine($"{DateTime.Now} Activated");
+            //write message to output on event
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine($"{DateTime.Now} Shown");
+            //write message to output on event
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine($"{DateTime.Now} Closing");
+            //write message to output on event
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine($"{DateTime.Now} Closed");
+            //write message to output on event
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine($"{DateTime.Now} Load");
+            //write message to output on event
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine($"{DateTime.Now} Paint");
+            //write message to output on event
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine($"{DateTime.Now} Deactivated");
+            //write message to output on event
+        }
+    }
+
+    
+}

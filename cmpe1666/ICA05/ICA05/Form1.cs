0d35090da8e866325e7ddb1734d050527655e978
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-01-20 23:26:38 +0000

    added comments

--- cmpe1666/ICA05/ICA05/Form1.cs
+++ cmpe1666/ICA05/ICA05/Form1.cs
@@ -16,24 +16,22 @@ namespace ICA05
         {
             InitializeComponent();
         }
-        const double m2k = 1.60934;
-        const double k2m = 0.621371;
-        private void val_Changed(object sender, EventArgs e)
+        const double m2k = 1.60934; //constant for converting miles to km from google search feature
+        const double k2m = 0.621371; //constant for converting km to miles from google search feature
+        private void val_Changed(object sender, EventArgs e) //consolidate radio buttons and text change into one event
         {
-            conversion();
+            conversion();//helper method
         }
 
         private void conversion()
         {
             double value, rate;
             if (mphButton.Checked)
-                rate = m2k;
+                rate = m2k;//if mph is the selected input (selected by default on form start)
             else
-                rate = k2m;
-            if (!double.TryParse(textBox1.Text, out value))
+                rate = k2m;//if kph is the selected input
+            if (!double.TryParse(textBox1.Text, out value))//if the entry in the input field is not a valid double, display an error message in the output
                 textBox2.Text = "Please enter a valid Input Speed.";
-            //if (textbox1.text != "")
-            //    messagebox.show("please enter a valid number.", "error: invalid input");
             else
                 textBox2.Text = $"{rate * value:F}";
 

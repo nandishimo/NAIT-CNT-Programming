d69f65cd354dcee8c85e8d26dcb4c002b97cdcbe
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2022-01-22 00:00:09 +0000

    base code done, automating position and adding helper methods

--- cmpe1666/Lab01/Lab01/Program.cs
+++ cmpe1666/Lab01/Lab01/Program.cs
@@ -65,51 +65,75 @@ namespace Lab01
 
 
             //draw bills and coins
+            /*count based 5x5 grid
+             *      count       position        coordinates                     equ'n 
+             *      0           1,1             100,100             x=100+count*400/2000 , y=100+count*100
+             *      1           1,2             100,200             x=100+count*400/2000 , y=100+count*100
+             *      2           1,3             100,300             x=100+count*400/2000 , y=100+count*100
+             *      3           1,4             100,400             x=100+count*400/2000 , y=100+count*100
+             *      4           1,5             100,500             x=100+count*400/2000 , y=100+count*100
+             *      5           2,1             500,100             x=100+count*400/2000 , y=100+count*100
+             *      6           2,2             500,200             x=100+count*400/2000 , y=100+count*100
+             *      7           2,3             500,300             x=100+count*400/2000 , y=100+count*100
+             *      8           2,4             500,400             x=100+count*400/2000 , y=100+count*100
+             *      9           2,5             500,500             x=100+count*400/2000 , y=100+count*100
+             *      if(count<5)
+             *          x=100
+             *          y=(count*100)+100
+             *      else
+             *          x=500
+             *          y=(count-4)*100
+            */ 
             int count = 0;
             if (fifty > 0)
             {
-                app.AddRectangle(50, 100 + 150 * count, 200, 100, Color.Red, 2, Color.Black);
+                //app.AddRectangle(50, 100 + 150 * count, 200, 100, Color.Red, 2, Color.Black);
+                drawBill(ref app, Color.Red, count, "Fifty", fifty);
                 count++;
             }
             if (twenty > 0)
             {
-                app.AddRectangle(50, 100 + 150 * count, 200, 100, Color.Green, 2, Color.Black);
+                //app.AddRectangle(50, 100 + 150 * count, 200, 100, Color.Green, 2, Color.Black);
+                drawBill(ref app, Color.LightGreen, count, "Twenty", twenty);
                 count++;
             }
             if (ten > 0)
             {
-                app.AddRectangle(50, 100 + 150 * count, 200, 100, Color.Purple, 2, Color.Black);
+                //app.AddRectangle(50, 100 + 150 * count, 200, 100, Color.Purple, 2, Color.Black);
+                drawBill(ref app, Color.MediumPurple, count, "Ten", ten);
                 count++;
             }
             if (five > 0)
             {
-                app.AddRectangle(50, 100 + 150 * count, 200, 100, Color.Blue, 2, Color.Black);
+                //app.AddRectangle(50, 100 + 150 * count, 200, 100, Color.Blue, 2, Color.Black);
+                drawBill(ref app, Color.LightBlue, count, "Five", five);
             }
             count = 0;
             if (toon > 0)
             {
-                app.AddEllipse(500, 100 * count, 100, 100, Color.Silver, 2, Color.Black);
-                app.AddEllipse(525, 25+100 * count, 50, 50, Color.Gold, 1, Color.Black);
+                //app.AddEllipse(500, 100 * count, 100, 100, Color.Silver, 2, Color.Black);
+                //app.AddEllipse(525, 25+100 * count, 50, 50, Color.Gold, 1, Color.Black);
+
                 count++;
             }
             if (loon > 0)
             {
-                app.AddEllipse(500, 100 * count, 100, 100, Color.Gold, 2, Color.Black);
+                //app.AddEllipse(500, 100 * count, 100, 100, Color.Gold, 2, Color.Black);
                 count++;
             }
             if (quart > 0)
             {
-                app.AddEllipse(500, 100 * count, 100, 100, Color.Silver, 2, Color.Black);
+                //app.AddEllipse(500, 100 * count, 100, 100, Color.Silver, 2, Color.Black);
                 count++;
             }
             if (dime > 0)
             {
-                app.AddEllipse(525, 25+100 * count, 50, 50, Color.Silver, 2, Color.Black);
+                //app.AddEllipse(525, 25+100 * count, 50, 50, Color.Silver, 2, Color.Black);
                 count++;
             }
             if (nick > 0)
             {
-                app.AddEllipse(525, 25 + 100 * count, 50, 50, Color.Gray, 2, Color.Black);
+                //app.AddEllipse(525, 25 + 100 * count, 50, 50, Color.Gray, 2, Color.Black);
             }
 
 
@@ -119,12 +143,38 @@ namespace Lab01
 
             Console.Read();
         }
-        private static void drawBill(ref CDrawer window, string denom, int qty)
+        private static void drawBill(ref CDrawer window, Color colour, int count, string denom, int qty)
         {
-            Point start = new Point(5, 5);
-            Size sBill = new Size(30, 15);
-            Rectangle bill = new Rectangle(start, sBill);
-            window.AddRectangle(bill, Color.Red, 2, Color.Black);
+            int x,y;
+            if (count < 5)
+            {
+                x = 100;
+                y = (count * 100) + 100;
+            }
+            else
+            {
+                x = 500;
+                y = (count - 4) * 100;
+            }
+            window.AddRectangle(new Rectangle(new Point(x, y), new Size(150, 75)), colour, 2, Color.Black);
+            window.AddText($"{denom} x {qty}", 20, new Rectangle(new Point(x, y), new Size(150, 75)));
+        }
+        private static void drawCoin(ref CDrawer window, Color colour, int count, string denom, int qty)
+        {
+            int x, y;
+            if (count < 5)
+            {
+                x = 100;
+                y = (count * 100) + 100;
+            }
+            else
+            {
+                x = 500;
+                y = (count - 4) * 100;
+            }
+            //window.AddRectangle(new Rectangle(new Point(x, y), new Size(150, 75)), colour, 2, Color.Black);
+            window.AddEllipse(x, y, 75, 75, colour, 2, Color.Black);
+            window.AddText($"{denom} x {qty}", 20, new Rectangle(new Point(x, y), new Size(150, 75)));
         }
     }
 }

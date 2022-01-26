e523d073e09679e039fc4e0131dbeef120a455bb
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2022-01-26 05:27:03 +0000

    final comments

--- cmpe1666/ICA06/ICA06/Form1.cs
+++ cmpe1666/ICA06/ICA06/Form1.cs
@@ -22,16 +22,19 @@ namespace ICA06
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text != "")
+            if (textBox1.Text != "")//only executes if textbox has something in it
             {
+                //add name from textbox to list and listbox
                 names.Add(textBox1.Text);
                 nameList.Items.Add(textBox1.Text);
+                //sort list and add disaply in other listbox
                 names.Sort();
                 sortedNamesList.Items.Clear();
                 foreach (string str in names)
                 {
                     sortedNamesList.Items.Add(str);
                 }
+                //clear current text from textbox
                 textBox1.Clear();
                 textBox1.Focus();
             }
@@ -40,28 +43,32 @@ namespace ICA06
 
         private void button2_Click(object sender, EventArgs e)
         {
+            //call recursive binary search for position of search text
             int position = -1;
             if (textBox1.Text != "")
                 position = listSearch(textBox1.Text,0,names.Count-1, names);
             if (position == -1)
-                MessageBox.Show($"{textBox1.Text} was not found in the list.");
+                MessageBox.Show($"{textBox1.Text} was not found in the list."); //message if no found (position -1)
             else
-                MessageBox.Show($"{textBox1.Text} was found at position {position+1} in the list.");
+                MessageBox.Show($"{textBox1.Text} was found at position {position+1} in the list."); //message if found
+            textBox1.Clear();//clear textbox after search
+            textBox1.Focus();
         }
 
         private int listSearch(string str, int low, int high, List<string> list)
         {
+            //set low and high (alphabetically) strings
             string first = list[low];
             string last = list[high];
             if (low > high)
                 return -1;
-            int mid = (low + high) / 2;
+            int mid = (low + high) / 2;//calc mid point of list
             if (str==list[mid])
-                return mid;
-            else if (String.Compare(str,list[low]) < 0)
-                return listSearch(str, low, mid - 1, list);
+                return mid;//return value when found
+            else if (String.Compare(str,list[low]) < 0)//
+                return listSearch(str, low, mid - 1, list);//call method with search limited to lower half
             else
-                return listSearch(str, mid+1, high, list);
+                return listSearch(str, mid+1, high, list);//call method with search limited to upper half
         }
     }
 }

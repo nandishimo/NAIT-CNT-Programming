c6c2f26ba49cb4a47946bcb7fe172d665a0b8ac1
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-01-27 00:25:42 +0000

    initial commit

---
+++ cmpe1666/ICA07/ICA07/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICA07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> list = new List<int>();
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            int.TryParse(numValBox.Text, out int num);
+            int.TryParse(minValBox.Text, out int min);
+            int.TryParse(min)
+            generateList(out list, num, min, max);
+        }
+
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

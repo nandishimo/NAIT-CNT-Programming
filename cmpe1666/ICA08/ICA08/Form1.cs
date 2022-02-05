157ff1348f986eef0be3d0255a37abb820522417
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-02-05 04:42:31 +0000

    finally got reading the file to work

--- cmpe1666/ICA08/ICA08/Form1.cs
+++ cmpe1666/ICA08/ICA08/Form1.cs
@@ -13,46 +13,65 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ICA08
 {
+    public struct Employee
+    {
+        public string id;
+        public string salary;
+
+
+    }
     public partial class Form1 : Form
     {
-        public struct Employee
-        {
-            public int id;
-            public int salary;
-            
-        }
-        public static void serializeEmployeeArray(Employee[] employees, string fileName)
-        {
-            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
-            BinaryFormatter bf = new BinaryFormatter();
-            bf.Serialize(fs, employees);
-            fs.Close();
-        }
 
-        public static Employee[] deserializationEmployeeArray(string fileName)
-        {
-            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-            BinaryFormatter bf = new BinaryFormatter();
-            Employee[] array = (Employee[])bf.Deserialize(fs);
-            fs.Close();
-            return (array);
-        }
 
+        
         public Form1()
         {
             InitializeComponent();
-            //List<Employee> employees.id ={ 28, 53, 12, 18, 8, 2, 19, 57, 62, 34, 23, 14, 48, 35, 55, 22, 26, 15, 7, 9, 32, 43, 41, 51 };
-            //int[] salaries = { 4500, 2800, 1900, 3100, 7000, 3500, 2200, 2800, 2850, 3150, 4000, 4500, 6000, 7200, 3700, 2100, 2450, 2500, 3250, 3700, 3800, 4200, 4100, 3900 };
+            List<string> providedIDs = new List<string>(){ "28", "53", "12", "18", "8", "2", "19", "57", "62", "34", "23", "14", "48", "35", "55", "22", "26", "15", "7", "9", "32", "43", "41", "51"};
+            List<string> providedSalary = new List<string>() { "4500", "2800", "1900", "3100", "7000", "3500", "2200", "2800", "2850", "3150", "4000", "4500", "6000", "7200", "3700", "2100", "2450", "2500", "3250", "3700", "3800", "4200", "4100", "3900"};
         }
-        Employee[] employees = new Employee[24];
+        List<Employee> employees = new List<Employee>();
+
+        public void fileRead(string fileName1, string fileName2, ref List<string> IDs, ref List<string> salary)
+        {
+            StreamReader sr1 = new StreamReader(fileName1);
+            StreamReader sr2 = new StreamReader(fileName2);
+            while (!sr1.EndOfStream)
+            {
+                IDs.Add(sr1.ReadLine());
+                salary.Add(sr2.ReadLine());
+            }
+
+        }
 
         private void loadButton_Click(object sender, EventArgs e)
         {
+            List<string> IDs = new List<string>();
+            List<string> salary = new List<string>();
             openFileDialog1.ShowDialog();
-            string fn = openFileDialog1.FileName;
-            Employee[] employees = new Employee[1000];
-            employees = deserializationEmployeeArray(fn);
-            
+            string fn1 = openFileDialog1.FileName;
+            openFileDialog2.ShowDialog();
+            string fn2 = openFileDialog2.FileName;
+            fileRead(fn1, fn2, ref IDs, ref salary);
+            List<Employee> employees = new List<Employee>();
+            for(int i = 0; i<IDs.Count; i++)
+            {
+                Employee newEmployee = new Employee();
+                newEmployee.id = IDs[i];
+                newEmployee.salary = salary[i];
+                employees.Add(newEmployee);
+            }
+            foreach(Employee emp in employees)
+            {
+                unsortedBox.Items.Add($"{emp.id}:\t{emp.salary}");
+            }
+
+        }
+
+        private void clearSortedButton_Click(object sender, EventArgs e)
+        {
+            sortedBox.ClearSelected();
         }
     }
 }

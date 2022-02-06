cfb107403bc63518fd4530be5c2949ed2a358503
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2022-02-06 21:06:54 +0000

    forms layout complete

--- cmpe1666/LAB02-PictureBox/LAB02-PictureBox/Form1.Designer.cs
+++ cmpe1666/LAB02-PictureBox/LAB02-PictureBox/Form1.Designer.cs
@@ -39,9 +39,10 @@ namespace LAB02_PictureBox
             this.loadButton = new System.Windows.Forms.Button();
             this.transformButton = new System.Windows.Forms.Button();
             this.adjustSlider = new System.Windows.Forms.TrackBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.slideLeft = new System.Windows.Forms.Label();
+            this.slideRight = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.slideValue = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.adjustSlider)).BeginInit();
@@ -52,9 +53,11 @@ namespace LAB02_PictureBox
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(776, 400);
+            this.pictureBox1.Size = new System.Drawing.Size(743, 438);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -65,10 +68,10 @@ namespace LAB02_PictureBox
             this.groupBox1.Controls.Add(this.tintButton);
             this.groupBox1.Controls.Add(this.bwButton);
             this.groupBox1.Controls.Add(this.contrastButton);
-            this.groupBox1.Location = new System.Drawing.Point(116, 440);
+            this.groupBox1.Location = new System.Drawing.Point(116, 478);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(158, 65);
-            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Modification Type";
             // 
@@ -78,8 +81,7 @@ namespace LAB02_PictureBox
             this.noiseButton.Location = new System.Drawing.Point(98, 39);
             this.noiseButton.Name = "noiseButton";
             this.noiseButton.Size = new System.Drawing.Size(52, 17);
-            this.noiseButton.TabIndex = 3;
-            this.noiseButton.TabStop = true;
+            this.noiseButton.TabIndex = 4;
             this.noiseButton.Text = "Noise";
             this.noiseButton.UseVisualStyleBackColor = true;
             // 
@@ -90,7 +92,6 @@ namespace LAB02_PictureBox
             this.tintButton.Name = "tintButton";
             this.tintButton.Size = new System.Drawing.Size(43, 17);
             this.tintButton.TabIndex = 2;
-            this.tintButton.TabStop = true;
             this.tintButton.Text = "Tint";
             this.tintButton.UseVisualStyleBackColor = true;
             // 
@@ -100,18 +101,18 @@ namespace LAB02_PictureBox
             this.bwButton.Location = new System.Drawing.Point(6, 39);
             this.bwButton.Name = "bwButton";
             this.bwButton.Size = new System.Drawing.Size(86, 17);
-            this.bwButton.TabIndex = 1;
-            this.bwButton.TabStop = true;
+            this.bwButton.TabIndex = 3;
             this.bwButton.Text = "Black & White";
             this.bwButton.UseVisualStyleBackColor = true;
             // 
             // contrastButton
             // 
             this.contrastButton.AutoSize = true;
+            this.contrastButton.Checked = true;
             this.contrastButton.Location = new System.Drawing.Point(6, 16);
             this.contrastButton.Name = "contrastButton";
             this.contrastButton.Size = new System.Drawing.Size(64, 17);
-            this.contrastButton.TabIndex = 0;
+            this.contrastButton.TabIndex = 1;
             this.contrastButton.TabStop = true;
             this.contrastButton.Text = "Contrast";
             this.contrastButton.UseVisualStyleBackColor = true;
@@ -120,18 +121,18 @@ namespace LAB02_PictureBox
             // 
             this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBar1.Location = new System.Drawing.Point(12, 418);
+            this.progressBar1.Location = new System.Drawing.Point(12, 456);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(776, 16);
+            this.progressBar1.Size = new System.Drawing.Size(743, 16);
             this.progressBar1.TabIndex = 3;
             // 
             // loadButton
             // 
             this.loadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.loadButton.Location = new System.Drawing.Point(12, 440);
+            this.loadButton.Location = new System.Drawing.Point(12, 478);
             this.loadButton.Name = "loadButton";
             this.loadButton.Size = new System.Drawing.Size(98, 30);
-            this.loadButton.TabIndex = 4;
+            this.loadButton.TabIndex = 0;
             this.loadButton.Text = "Load Picture";
             this.loadButton.UseVisualStyleBackColor = true;
             this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
@@ -139,10 +140,10 @@ namespace LAB02_PictureBox
             // transformButton
             // 
             this.transformButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.transformButton.Location = new System.Drawing.Point(690, 440);
+            this.transformButton.Location = new System.Drawing.Point(657, 478);
             this.transformButton.Name = "transformButton";
             this.transformButton.Size = new System.Drawing.Size(98, 30);
-            this.transformButton.TabIndex = 5;
+            this.transformButton.TabIndex = 3;
             this.transformButton.Text = "Transform";
             this.transformButton.UseVisualStyleBackColor = true;
             // 
@@ -150,48 +151,63 @@ namespace LAB02_PictureBox
             // 
             this.adjustSlider.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.adjustSlider.Location = new System.Drawing.Point(280, 440);
+            this.adjustSlider.Location = new System.Drawing.Point(280, 478);
             this.adjustSlider.Maximum = 20;
             this.adjustSlider.Name = "adjustSlider";
-            this.adjustSlider.Size = new System.Drawing.Size(404, 45);
-            this.adjustSlider.TabIndex = 6;
+            this.adjustSlider.Size = new System.Drawing.Size(371, 45);
+            this.adjustSlider.TabIndex = 2;
             this.adjustSlider.Value = 10;
+            this.adjustSlider.Scroll += new System.EventHandler(this.adjustSlider_Scroll);
             // 
-            // label1
+            // slideLeft
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(280, 472);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(21, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "left";
+            this.slideLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.slideLeft.AutoSize = true;
+            this.slideLeft.Location = new System.Drawing.Point(280, 510);
+            this.slideLeft.Name = "slideLeft";
+            this.slideLeft.Size = new System.Drawing.Size(29, 13);
+            this.slideLeft.TabIndex = 7;
+            this.slideLeft.Text = "Less";
             // 
-            // label2
+            // slideRight
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(657, 472);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(27, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "right";
+            this.slideRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.slideRight.AutoSize = true;
+            this.slideRight.Location = new System.Drawing.Point(624, 510);
+            this.slideRight.Name = "slideRight";
+            this.slideRight.Size = new System.Drawing.Size(31, 13);
+            this.slideRight.TabIndex = 8;
+            this.slideRight.Text = "More";
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // slideValue
+            // 
+            this.slideValue.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.slideValue.AutoSize = true;
+            this.slideValue.Location = new System.Drawing.Point(446, 510);
+            this.slideValue.Name = "slideValue";
+            this.slideValue.Size = new System.Drawing.Size(35, 13);
+            this.slideValue.TabIndex = 9;
+            this.slideValue.Text = "label3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 524);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(767, 562);
+            this.Controls.Add(this.slideValue);
+            this.Controls.Add(this.slideRight);
+            this.Controls.Add(this.slideLeft);
             this.Controls.Add(this.adjustSlider);
             this.Controls.Add(this.transformButton);
             this.Controls.Add(this.loadButton);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
+            this.MinimumSize = new System.Drawing.Size(560, 440);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -215,9 +231,10 @@ namespace LAB02_PictureBox
         private System.Windows.Forms.Button loadButton;
         private System.Windows.Forms.Button transformButton;
         private System.Windows.Forms.TrackBar adjustSlider;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label slideLeft;
+        private System.Windows.Forms.Label slideRight;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label slideValue;
     }
 }
 

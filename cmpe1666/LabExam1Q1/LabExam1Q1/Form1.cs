bbd5e27a2305e69fe2b23e8952cfcfb85b604411
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-02-09 20:10:41 +0000

    initial commit

---
+++ cmpe1666/LabExam1Q1/LabExam1Q1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabExam1Q1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void valueChanged(object sender, EventArgs e)
+        {
+            int.TryParse(value1Box.Text, out int value1);
+            int.TryParse(value2Box.Text, out int value2);
+            if (multiplyRB.Checked)
+            {
+                resultsBox.Text = $"{value1 * value2}";
+            }
+            else if (addRB.Checked)
+            {
+                resultsBox.Text = $"{value1 + value2}";
+            }
+            else
+            {
+                resultsBox.Text = $"{value1 - value2}";
+            }
+        }
+    }
+}

1e3422db670325fdd30c3909613dd8ed302aaa3a
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-05-17 20:23:39 +0000

    seems good

--- cmpe1666/CMPE1666-LE3-Q2/CMPE1666-LE3-Q2/Form1.Designer.cs
+++ cmpe1666/CMPE1666-LE3-Q2/CMPE1666-LE3-Q2/Form1.Designer.cs
@@ -29,13 +29,98 @@ namespace CMPE1666_LE3_Q2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.UI_Track_MaxCount = new System.Windows.Forms.TrackBar();
+            this.UI_BTN_Start = new System.Windows.Forms.Button();
+            this.LBL_Min = new System.Windows.Forms.Label();
+            this.LBL_Max = new System.Windows.Forms.Label();
+            this.UI_BTN_Stop = new System.Windows.Forms.Button();
+            this.UI_LBL_Count = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_Track_MaxCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UI_Track_MaxCount
+            // 
+            this.UI_Track_MaxCount.Location = new System.Drawing.Point(12, 12);
+            this.UI_Track_MaxCount.Minimum = 1;
+            this.UI_Track_MaxCount.Name = "UI_Track_MaxCount";
+            this.UI_Track_MaxCount.Size = new System.Drawing.Size(461, 45);
+            this.UI_Track_MaxCount.TabIndex = 0;
+            this.UI_Track_MaxCount.Value = 1;
+            // 
+            // UI_BTN_Start
+            // 
+            this.UI_BTN_Start.Location = new System.Drawing.Point(12, 63);
+            this.UI_BTN_Start.Name = "UI_BTN_Start";
+            this.UI_BTN_Start.Size = new System.Drawing.Size(75, 23);
+            this.UI_BTN_Start.TabIndex = 1;
+            this.UI_BTN_Start.Text = "Start";
+            this.UI_BTN_Start.UseVisualStyleBackColor = true;
+            this.UI_BTN_Start.Click += new System.EventHandler(this.UI_BTN_Start_Click);
+            // 
+            // LBL_Min
+            // 
+            this.LBL_Min.AutoSize = true;
+            this.LBL_Min.Location = new System.Drawing.Point(21, 44);
+            this.LBL_Min.Name = "LBL_Min";
+            this.LBL_Min.Size = new System.Drawing.Size(13, 13);
+            this.LBL_Min.TabIndex = 2;
+            this.LBL_Min.Text = "1";
+            // 
+            // LBL_Max
+            // 
+            this.LBL_Max.AutoSize = true;
+            this.LBL_Max.Location = new System.Drawing.Point(454, 44);
+            this.LBL_Max.Name = "LBL_Max";
+            this.LBL_Max.Size = new System.Drawing.Size(19, 13);
+            this.LBL_Max.TabIndex = 3;
+            this.LBL_Max.Text = "10";
+            // 
+            // UI_BTN_Stop
+            // 
+            this.UI_BTN_Stop.Location = new System.Drawing.Point(93, 63);
+            this.UI_BTN_Stop.Name = "UI_BTN_Stop";
+            this.UI_BTN_Stop.Size = new System.Drawing.Size(75, 23);
+            this.UI_BTN_Stop.TabIndex = 4;
+            this.UI_BTN_Stop.Text = "Stop";
+            this.UI_BTN_Stop.UseVisualStyleBackColor = true;
+            this.UI_BTN_Stop.Click += new System.EventHandler(this.UI_BTN_Stop_Click);
+            // 
+            // UI_LBL_Count
+            // 
+            this.UI_LBL_Count.AutoSize = true;
+            this.UI_LBL_Count.Location = new System.Drawing.Point(207, 68);
+            this.UI_LBL_Count.Name = "UI_LBL_Count";
+            this.UI_LBL_Count.Size = new System.Drawing.Size(13, 13);
+            this.UI_LBL_Count.TabIndex = 5;
+            this.UI_LBL_Count.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(497, 106);
+            this.Controls.Add(this.UI_LBL_Count);
+            this.Controls.Add(this.UI_BTN_Stop);
+            this.Controls.Add(this.LBL_Max);
+            this.Controls.Add(this.LBL_Min);
+            this.Controls.Add(this.UI_BTN_Start);
+            this.Controls.Add(this.UI_Track_MaxCount);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.UI_Track_MaxCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TrackBar UI_Track_MaxCount;
+        private System.Windows.Forms.Button UI_BTN_Start;
+        private System.Windows.Forms.Label LBL_Min;
+        private System.Windows.Forms.Label LBL_Max;
+        private System.Windows.Forms.Button UI_BTN_Stop;
+        private System.Windows.Forms.Label UI_LBL_Count;
     }
 }
 
--- cmpe1666/CMPE1666-LE3-Q2/CMPE1666-LE3-Q2/Form1.cs
+++ cmpe1666/CMPE1666-LE3-Q2/CMPE1666-LE3-Q2/Form1.cs
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace CMPE1666_LE3_Q2
 {
+    delegate void delvoidint(int x);
+    delegate void delvoidvoid();
     public partial class Form1 : Form
     {
+        Thread th1 = null;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void UI_BTN_Start_Click(object sender, EventArgs e)
+        {
+            UI_BTN_Start.Enabled = false;
+            th1 = new Thread(new ParameterizedThreadStart(Count));
+            th1.Start(UI_Track_MaxCount.Value);
+        }
+        private void Count(object objData)
+        {
+            int max = 0;
+            if (objData is int)
+            {
+                int i = 0;
+                max = (int)objData;
+                for (i = 1; i < max; i++)
+                {
+                    Invoke(new delvoidint(PushCountToLabel), i);
+                    Thread.Sleep(500);
+                }
+                Invoke(new delvoidint(PushCountToLabel), i);
+            }
+            Invoke(new delvoidvoid(EnableStart));
+
+        }
+        private void PushCountToLabel(int count)
+        {
+            UI_LBL_Count.Text = count.ToString();
+        }
+        private void EnableStart()
+        {
+            UI_BTN_Start.Enabled = true;
+        }
+
+        private void UI_BTN_Stop_Click(object sender, EventArgs e)
+        {
+            if (th1.IsAlive)
+            {
+                th1.Abort();
+                Invoke(new delvoidvoid(EnableStart));
+
+            }
         }
     }
 }

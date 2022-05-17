c028dd713ce9b4a06ae38e70037161367edc5387
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2022-05-17 03:02:56 +0000

    added comments

--- cmpe1666/ICA13/ICA13/Form1.cs
+++ cmpe1666/ICA13/ICA13/Form1.cs
@@ -9,35 +9,37 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Threading;
-
+/*Made by Nandish Patel
+ *May 2022
+ */
 namespace ICA13
 {
-    delegate void delListToTB(TextBox tb, List<string> list);
+    delegate void delListToTB(TextBox tb, List<string> list);//delegate to take tb element and list of strings
     public partial class Form1 : Form
     {
-        delListToTB _dDisplayPalindromes = null;
-        Thread th1 = null;
-        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        delListToTB _dDisplayPalindromes = null; //create instance of delegate to display palindromes
+        Thread th1 = null; //thread for find palindromes method
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch(); //stopwatch to time palindrome finding
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private bool isPalindrome(string word)
+        private bool isPalindrome(string word) //check word if palindrome
         {
             int length = word.Length;
             if (length <= 1)
                 return true;
             if (word[0] == word[length - 1])
             {
-                return isPalindrome(word.Substring(1, length - 2));
+                return isPalindrome(word.Substring(1, length - 2));//recursively check if ith and (n-ith) letter are the same
             }
             else
                 return false;
         }
 
-        private void findPalindromes()
+        private void findPalindromes()//check if palindrome, but for a whole list of strings this time.
         {
             _dDisplayPalindromes = new delListToTB(PushListToTB);
             List<string> palindromes = new List<string>();
@@ -46,18 +48,18 @@ namespace ICA13
                 if (isPalindrome(word))
                     palindromes.Add(word);
             }
-            Invoke(_dDisplayPalindromes, UI_Loaded_TB, palindromes);
+            Invoke(_dDisplayPalindromes, UI_Loaded_TB, palindromes);//push found palindromes to display after complete.
         }
         List<string> wordString = null;
         private void UI_Load_btn_Click(object sender, EventArgs e)
         {
             UI_OFD.ShowDialog();
-            wordString = new List<string>(File.ReadAllLines(UI_OFD.FileName));
+            wordString = new List<string>(File.ReadAllLines(UI_OFD.FileName)); //read words from file and load into list fo strings
             UI_Loaded_TB.Text = $"Loaded {wordString.Count()} words!";
         }
 
         private void UI_Find_btn_Click(object sender, EventArgs e)
-        {
+        {//restart sw and start background thread to find all palindromes loaded
             sw.Restart();
             th1 = new Thread(findPalindromes);
             th1.IsBackground = true;
@@ -65,7 +67,7 @@ namespace ICA13
         }
 
         private void UI_Test_btn_Click(object sender, EventArgs e)
-        {
+        { //test single words from textbox 
             string message = "";
             if (UI_Test_TB.Text == "")
                 return;
@@ -81,7 +83,7 @@ namespace ICA13
         }
 
         private void PushListToTB(TextBox tb, List<string> list)
-        {
+        { //method to display a list of strings in a textbox
             string str = "";
             foreach(string word in list)
             {

e9e48f29c2162c2e8059ccaed9a44149e3608900
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2022-05-17 00:01:09 +0000

    seems good

--- cmpe1666/ICA14/ICA14/Form1.cs
+++ cmpe1666/ICA14/ICA14/Form1.cs
@@ -11,8 +11,26 @@ using System.Threading;
 
 namespace ICA14
 {
+    delegate void delvoidvoid();
+    delegate void delvoidstring(string s);
     public partial class Form1 : Form
     {
+        //delvoidvoid _delColorAnalyzer = null;
+        //Thread th1 = null;
+        List<Thread> thList = new List<Thread>();
+        List<string> pictures = null;
+        struct PictureBM
+        {
+            public Bitmap bitmap;
+            public string file;
+
+            public PictureBM(Bitmap bm, string fname)
+            {
+                bitmap = bm;
+                file = fname;
+            }
+
+        }
         public Form1()
         {
             InitializeComponent();
@@ -20,9 +38,85 @@ namespace ICA14
 
         private void UI_btn_Go_Click(object sender, EventArgs e)
         {
-            openFileDialog1.ShowDialog();
+            //_delColorAnalyzer = new delvoidvoid()
+            //th1 = new Thread(AnalyzeColour);
+            //th1.IsBackground = true;
+            //Thread newThread = new Thread(new ParameterizedThreadStart(AnalyzeColour));
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                timer1.Enabled=true;
+                Invoke(new delvoidstring(PushItemToTextBox),"Starting...");
+                pictures = new List<string>(openFileDialog1.FileNames);
+                foreach (string fname in pictures)
+                {
+                    Bitmap bm = (Bitmap)Bitmap.FromFile(fname);
+                    thList.Add(new Thread(new ParameterizedThreadStart(AnalyzeColour)));
+                    thList.Last().IsBackground = true;
+                    thList.Last().Start(new PictureBM(bm,fname));
+                    
+                }
+                //CheckThreadDone();
+                
+            }
+        }
+        private bool CheckThreadDone()
+        {
+            bool threadDone = true;
+            foreach(Thread th in thList)
+            {
+                if(th.ThreadState==ThreadState.Background)
+                    threadDone = false;
+            }
+            return threadDone;
+        }
+        private void AnalyzeAllPictures()
+        {
+            foreach (string fname in pictures)
+            {
+                Bitmap bm = (Bitmap)Bitmap.FromFile(fname);
+                AnalyzeColour(bm);
+            }
         }
 
-        private void 
+        public void AnalyzeColour(object objData)
+        {
+            int red=0;
+            int green=0;
+            int blue=0;
+            string fname = "";
+            if(objData is PictureBM)
+            {
+                PictureBM bm = (PictureBM)objData;
+                fname = bm.file;
+                for (int i = 0; i < bm.bitmap.Width; i++)
+                {
+                    for (int j = 0; j < bm.bitmap.Height; j++)
+                    {
+                        red += bm.bitmap.GetPixel(i, j).R;
+                        green += bm.bitmap.GetPixel(i, j).G;
+                        blue += bm.bitmap.GetPixel(i, j).B;
+                    }
+                }
+            }
+            int sum = red + green + blue;
+            float rpercent = (float)red / sum;
+            float gpercent = (float)green / sum;
+            float bpercent = (float)blue / sum;
+            Invoke(new delvoidstring(PushItemToTextBox), $"(R:{100 * rpercent:F1}%, G:{100 * gpercent:F1}%, B:{100 * bpercent:F1}%):{fname}");
+        }
+        private void PushItemToTextBox(string s)
+        {
+            UI_lb_Text.Items.Add(s);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckThreadDone())
+            {
+                Invoke(new delvoidstring(PushItemToTextBox), "Done!");
+                timer1.Stop();
+            }
+        }
     }
 }

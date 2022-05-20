8a6998a8833aa34b42f7735c4b396e9c046efb95
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-05-20 21:39:05 +0000

    seems good

--- cmpe1666/ICA16/ICA16/Form1.Designer.cs
+++ cmpe1666/ICA16/ICA16/Form1.Designer.cs
@@ -29,15 +29,8 @@ namespace ICA16
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.UI_TB_Queue = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // timer1
-            // 
-            this.timer1.Enabled = true;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // UI_TB_Queue
             // 
@@ -46,6 +39,7 @@ namespace ICA16
             this.UI_TB_Queue.ReadOnly = true;
             this.UI_TB_Queue.Size = new System.Drawing.Size(775, 20);
             this.UI_TB_Queue.TabIndex = 0;
+            this.UI_TB_Queue.Text = "0 segs in queue. Estimated time to draw: 0.00 seconds";
             // 
             // Form1
             // 
@@ -55,14 +49,13 @@ namespace ICA16
             this.Controls.Add(this.UI_TB_Queue);
             this.Name = "Form1";
             this.Text = "ICA 16 - Slow Draw";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TextBox UI_TB_Queue;
     }
 }

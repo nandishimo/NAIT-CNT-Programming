331cffcf2cec78fe768ba720f12cb3c9dc2b1356
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-09-08 14:48:18 +0000

    showing dialog

--- cmpe2300/DEMO/NandishPatel_Demo01/NandishPatel_Demo01/DemoDialog.cs
+++ cmpe2300/DEMO/NandishPatel_Demo01/NandishPatel_Demo01/DemoDialog.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Diagnostics.Trace;
 
 namespace NandishPatel_Demo01
 {
   public partial class DemoDialog : Form
   {
+    Timer timer = new Timer();
+    Random _rnd = null; //member, unallocated as Random
+
+    //delegates/events
+    public delegate void dColorHandler(object sender, Color color); //delegate/event type
+    public event dColorHandler _GetColor = null; //default to null
     public DemoDialog()
     {
       InitializeComponent();
+      WriteLine("Starting...");
+      Text = "DemoDialog";
+      _rnd = new Random();
+
+      timer.Interval = 2000;
+      timer.Enabled = true;
+      timer.Tick += TimerCallback;
+
+      Click += DemoDialog_Click;
+    }
+
+    private void DemoDialog_Click(object sender, EventArgs e)
+    {
+      WriteLine("DemoDialog_Click");
+    }
+
+    private void TimerCallback(object sender, EventArgs e)
+    {
+      Text = DateTime.Now.ToShortTimeString();
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
     }
   }
 }

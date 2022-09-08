b8a31b9a03c0646e6d623df084a4ef562c02fa59
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-09-08 15:51:58 +0000

    more stuff

--- cmpe2300/DEMO/NandishPatel_Demo01/NandishPatel_Demo01/Form1.Designer.cs
+++ cmpe2300/DEMO/NandishPatel_Demo01/NandishPatel_Demo01/Form1.Designer.cs
@@ -28,81 +28,87 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.label4 = new System.Windows.Forms.Label();
-      this.lbl_Wheel = new System.Windows.Forms.Label();
-      this.label2 = new System.Windows.Forms.Label();
-      this._pb = new System.Windows.Forms.PictureBox();
       this.lbl_Capture = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.lbl_Wheel = new System.Windows.Forms.Label();
+      this._pb = new System.Windows.Forms.PictureBox();
+      this.label1 = new System.Windows.Forms.Label();
       ((System.ComponentModel.ISupportInitialize)(this._pb)).BeginInit();
       this.SuspendLayout();
       // 
-      // label4
+      // lbl_Capture
       // 
-      this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+      this.lbl_Capture.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
-      this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label4.Location = new System.Drawing.Point(175, 9);
-      this.label4.Name = "label4";
-      this.label4.Size = new System.Drawing.Size(237, 39);
-      this.label4.TabIndex = 9;
-      this.label4.Text = "label4";
+      this.lbl_Capture.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.lbl_Capture.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lbl_Capture.Location = new System.Drawing.Point(139, 7);
+      this.lbl_Capture.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.lbl_Capture.Name = "lbl_Capture";
+      this.lbl_Capture.Size = new System.Drawing.Size(175, 32);
+      this.lbl_Capture.TabIndex = 9;
+      this.lbl_Capture.Text = "label4";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label3.Location = new System.Drawing.Point(9, 50);
+      this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(105, 31);
+      this.label3.TabIndex = 8;
+      this.label3.Text = "Wheel :";
       // 
       // lbl_Wheel
       // 
-      this.lbl_Wheel.AutoSize = true;
+      this.lbl_Wheel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
       this.lbl_Wheel.BackColor = System.Drawing.SystemColors.ActiveCaption;
       this.lbl_Wheel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.lbl_Wheel.Location = new System.Drawing.Point(12, 62);
+      this.lbl_Wheel.Location = new System.Drawing.Point(139, 50);
+      this.lbl_Wheel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
       this.lbl_Wheel.Name = "lbl_Wheel";
-      this.lbl_Wheel.Size = new System.Drawing.Size(127, 38);
-      this.lbl_Wheel.TabIndex = 8;
-      this.lbl_Wheel.Text = "Wheel :";
-      // 
-      // label2
-      // 
-      this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-      this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-      this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label2.Location = new System.Drawing.Point(175, 62);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(237, 39);
-      this.label2.TabIndex = 7;
-      this.label2.Text = "label2";
+      this.lbl_Wheel.Size = new System.Drawing.Size(175, 32);
+      this.lbl_Wheel.TabIndex = 7;
+      this.lbl_Wheel.Text = "label2";
       // 
       // _pb
       // 
       this._pb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this._pb.Location = new System.Drawing.Point(12, 140);
+      this._pb.Location = new System.Drawing.Point(9, 114);
+      this._pb.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this._pb.Name = "_pb";
-      this._pb.Size = new System.Drawing.Size(400, 312);
+      this._pb.Size = new System.Drawing.Size(305, 254);
       this._pb.TabIndex = 6;
       this._pb.TabStop = false;
       // 
-      // lbl_Capture
+      // label1
       // 
-      this.lbl_Capture.AutoSize = true;
-      this.lbl_Capture.BackColor = System.Drawing.SystemColors.ActiveCaption;
-      this.lbl_Capture.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.lbl_Capture.Location = new System.Drawing.Point(12, 9);
-      this.lbl_Capture.Name = "lbl_Capture";
-      this.lbl_Capture.Size = new System.Drawing.Size(151, 38);
-      this.lbl_Capture.TabIndex = 5;
-      this.lbl_Capture.Text = "Capture :";
+      this.label1.AutoSize = true;
+      this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(9, 7);
+      this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(126, 31);
+      this.label1.TabIndex = 5;
+      this.label1.Text = "Capture :";
       // 
       // Form1
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(424, 464);
-      this.Controls.Add(this.label4);
-      this.Controls.Add(this.lbl_Wheel);
-      this.Controls.Add(this.label2);
-      this.Controls.Add(this._pb);
+      this.ClientSize = new System.Drawing.Size(323, 377);
       this.Controls.Add(this.lbl_Capture);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.lbl_Wheel);
+      this.Controls.Add(this._pb);
+      this.Controls.Add(this.label1);
+      this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this.Name = "Form1";
       this.Text = "Form1";
       ((System.ComponentModel.ISupportInitialize)(this._pb)).EndInit();
@@ -113,11 +119,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label lbl_Wheel;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.PictureBox _pb;
         private System.Windows.Forms.Label lbl_Capture;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_Wheel;
+        private System.Windows.Forms.PictureBox _pb;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- cmpe2300/DEMO/NandishPatel_Demo01/NandishPatel_Demo01/Form1.cs
+++ cmpe2300/DEMO/NandishPatel_Demo01/NandishPatel_Demo01/Form1.cs
@@ -8,18 +8,76 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Diagnostics.Trace;
 
 namespace NandishPatel_Demo01
 {
   public partial class Form1 : Form
   {
     DemoDialog _demoDialog = null;
+    int _iWheelValue = 0;
+    int _iCaptureValue = 100;
+    int _iCaptureXStart = 0;
+    int _iTempCapture = 0;
+
     public Form1()
     {
       InitializeComponent();
       Text = "Demo01";
       _pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
       Shown += Form1_Shown;
+      MouseWheel += Form1_MouseWheel;//not needed, shows rejection
+      lbl_Wheel.MouseWheel += Form1_MouseWheel;
+      lbl_Capture.MouseDown += Lbl_Capture_MouseDown;
+      lbl_Capture.MouseMove += Lbl_Capture_MouseMove;
+      lbl_Capture.MouseUp += Lbl_Capture_MouseUp;
+      lbl_Capture.Text = _iCaptureValue.ToString();
+      Click += Form1_Click;
+      AllowDrop = true;
+      _pb.DragEnter += _pb_DragEnter;
+      _pb.DragDrop += _pb_DragDrop;
+    }
+
+    private void _pb_DragDrop(object sender, DragEventArgs e)
+    {
+      //throw new NotImplementedException();
+    }
+
+    private void _pb_DragEnter(object sender, DragEventArgs e)
+    {
+      //e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop);
+    }
+
+    private void Form1_Click(object sender, EventArgs e)
+    {
+      OpenFileDialog ofd = new OpenFileDialog();
+    }
+
+    private void Lbl_Capture_MouseUp(object sender, MouseEventArgs e)
+    {
+      _iCaptureValue = _iTempCapture;
+    }
+
+    private void Lbl_Capture_MouseMove(object sender, MouseEventArgs e)
+    {
+      //WriteLine(e.X.ToString());
+      if (!lbl_Capture.Capture) return;
+      _iTempCapture += (e.X-_iCaptureXStart)/10;
+      lbl_Capture.Text = _iTempCapture.ToString();
+    }
+
+    private void Lbl_Capture_MouseDown(object sender, MouseEventArgs e)
+    {
+      _iCaptureXStart = e.X; //save start position of mouse down
+    }
+
+    private void Form1_MouseWheel(object sender, MouseEventArgs e)
+    {
+      if (sender != lbl_Wheel) return; //reject if not sent by wheel label
+      if (sender is Form1) return; //reject if sender is of type Form
+      //WriteLine(e.Delta.ToString());//+120 or -120
+      _iWheelValue += e.Delta > 0 ? 1 : -1;
+      lbl_Wheel.Text = _iWheelValue.ToString();
     }
 
     private void Form1_Shown(object sender, EventArgs e)
@@ -30,11 +88,22 @@ namespace NandishPatel_Demo01
       _demoDialog.StartPosition = FormStartPosition.Manual;
       _demoDialog.Location = new Point(this.Left + this.Width, this.Top);
       _demoDialog.Show();
+      DemoDialog _dd2 = new DemoDialog();
+      _dd2._GetColor += _demoDialog__GetColor;//subscribe to dd event as well
+      _dd2.Show();
     }
 
     private void _demoDialog__GetColor(object sender, Color color)
     {
       BackColor = color;
+      //unsubscribe literally
+      //_demoDialog._GetColor -= _demoDialog__GetColor;
+
+      //unsubscribe from whoever called
+      if(sender is DemoDialog dd)
+      {
+        dd._GetColor -= _demoDialog__GetColor;  
+      }
     }
   }
 }

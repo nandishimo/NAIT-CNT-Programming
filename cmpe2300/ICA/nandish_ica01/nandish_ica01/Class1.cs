398d08f5e81550011079a2fcae6ff5f7dc942da1
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2022-09-13 17:51:23 +0000

    TrekLamp class added. form 1 code in progress

---
+++ cmpe2300/ICA/nandish_ica01/nandish_ica01/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using static System.Diagnostics.Trace;
+using GDIDrawer;
+
+/***********************************
+*Nandish Patel
+*CMPE2300
+*Submission Code : 1221_2300_A01
+***********************************/
+
+namespace nandish_ica01
+{
+  internal class TrekLamp
+  {
+    private Color _LampColor;
+    private byte _byToggle;
+    private byte _byTick;
+    private int _border;
+    public TrekLamp(byte byToggle, Color LampColor, int border=2)
+    {
+      _byToggle = byToggle;
+      _LampColor = LampColor;
+      _border = border;
+      Random random = new Random();
+      _byTick = RandColor.GetColor().R;
+    }
+    public TrekLamp() : this(64,RandColor.GetColor(),6)
+    {
+      //default constructor
+    }
+    private void TickLamp()
+    {
+      _byTick+=3;
+    }
+    private void RedAlert(bool state)
+    {
+      if (state)
+      {
+        _byTick = _byToggle;
+      }
+      else
+        _byTick = RandColor.GetColor().R;
+    }
+    private void RenderLamp(ref CDrawer window, int lamp)
+    {
+      if (_byTick > _byToggle)
+      {
+        window.AddRectangle(lamp % window.ScaledWidth, lamp / window.ScaledWidth, 1, 1, _LampColor, _border);
+      }
+
+      
+    }
+    
+  }
+}

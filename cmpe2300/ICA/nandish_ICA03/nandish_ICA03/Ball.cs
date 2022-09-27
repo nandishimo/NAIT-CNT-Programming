2ee1bd5846d3b0c7dedba2b236978a94b4d033b2
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-09-27 16:40:16 +0000

    adjusted math for ball position

--- cmpe2300/ICA/nandish_ICA03/nandish_ICA03/Ball.cs
+++ cmpe2300/ICA/nandish_ICA03/nandish_ICA03/Ball.cs
@@ -40,8 +40,8 @@ namespace nandish_ICA03
     {
       _color = RandColor.GetKnownColor();
       _velocity = new Vector2((float)(rand.NextDouble()-0.5)*20, (float)(rand.NextDouble() - 0.5) * 20);
-      double x = Radius / 2 +(window.ScaledWidth - Radius)*rand.NextDouble();
-      double y = Radius / 2 + (window.ScaledHeight - Radius) * rand.NextDouble();
+      double x = _radius +(window.ScaledWidth - 2*_radius) * rand.NextDouble();
+      double y = _radius + (window.ScaledHeight - 2*_radius) * rand.NextDouble();
       _point = new PointF((float)x,(float)y);
     }
     public static Point DrawerLocation
@@ -52,7 +52,7 @@ namespace nandish_ICA03
     public void ShowBall()
     { //add ball to drawer. used iAlive as opacity so it fades out
       Color color = Color.FromArgb(_iAlive, _color);
-      window.AddCenteredEllipse((int)_point.X,(int)_point.Y, _radius, _radius, color);
+      window.AddCenteredEllipse((int)_point.X,(int)_point.Y, _radius*2, _radius*2, color);
     }
 
     public void MoveBall() //adjust position of ball based on velocity and alive counter
@@ -60,32 +60,32 @@ namespace nandish_ICA03
       _iAlive--;
       if (_iAlive < 1)
       {// count down iAlive so ball fades out, then set new random position
-        double x = Radius / 2 + (window.ScaledWidth - Radius) * rand.NextDouble();
-        double y = Radius / 2 + (window.ScaledHeight - Radius) * rand.NextDouble();
+        double x = _radius + (window.ScaledWidth - 2 * _radius) * rand.NextDouble();
+        double y = _radius + (window.ScaledHeight - 2 * _radius) * rand.NextDouble();
         _point = new PointF((float)x, (float)y);
         _iAlive = rand.Next(50, 128);
       }
       //move ball by added velocity to position, bound ball position to drawer size so it bounces
       PointF tmp = new PointF(_point.X+_velocity.X, _point.Y+_velocity.Y);
-      if (tmp.X < _radius / 2)
+      if (tmp.X < _radius)
       {
         _velocity.X *= -1;
-        tmp.X = _radius / 2;
+        tmp.X = _radius;
       }
-      else if(tmp.X > (window.ScaledWidth - _radius/2))
+      else if(tmp.X > (window.ScaledWidth - _radius))
       {
         _velocity.X *= -1;
-        tmp.X = (window.ScaledWidth-_radius/2);
+        tmp.X = (window.ScaledWidth-_radius);
       }
-      if (tmp.Y < _radius / 2)
+      if (tmp.Y < _radius)
       {
         _velocity.Y *= -1;
-        tmp.Y = _radius / 2;
+        tmp.Y = _radius;
       }
-      else if(tmp.Y > (window.ScaledHeight - _radius / 2))
+      else if(tmp.Y > (window.ScaledHeight - _radius))
       {
         _velocity.Y *= -1;
-        tmp.Y = (window.ScaledHeight-_radius/2);
+        tmp.Y = (window.ScaledHeight-_radius);
       }
       _point = tmp; //set position to adjusted temp point
     }

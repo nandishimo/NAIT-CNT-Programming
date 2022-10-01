1fe744fa8d42cb671559cd539768a7e88891c38c
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2022-10-01 07:40:54 +0000

    animation with timer implemented

--- cmpe2300/LAB/Nandish_LAB01/Nandish_LAB01/TileDialog.cs
+++ cmpe2300/LAB/Nandish_LAB01/Nandish_LAB01/TileDialog.cs
@@ -1,7 +1,7 @@
 ﻿/***********************************
 *Nandish Patel
 *CMPE2300
-*Submission Code : 1221_2300_A03
+*Submission Code : 1221_2300_L01
 ***********************************/
 using System;
 using System.Collections.Generic;
@@ -23,6 +23,7 @@ namespace Nandish_LAB01
       _close
     }
     public delegate void TurnHandler(hideOrClose hoc);
+    public event TurnHandler _turntile = null;
     public event EventHandler _guessClick = null;
     public event EventHandler _turnComplete = null;
     public string Secret
@@ -68,8 +69,9 @@ namespace Nandish_LAB01
       Cheat = false;
       _lblChar.Font = new Font("Sans Serif", fontSize);
       timer.Interval = 20;
-      _lblChar.BackColor = _lblChar.ForeColor = Color.White;
-      Click += TileDialog_Click;
+      _lblChar.BackColor = _lblChar.ForeColor = BackColor;
+      //Click += TileDialog_Click;
+      _lblChar.Click += TileDialog_Click;
       timer.Tick += Timer_Tick;
       _lblChar.MouseEnter += _lblChar_MouseEnter;
       _lblChar.MouseLeave += _lblChar_MouseLeave;
@@ -87,35 +89,46 @@ namespace Nandish_LAB01
       }
       else if(hoc == hideOrClose._close)
       {
-        Invoke(_turnComplete);
+        _turnComplete?.Invoke(this, EventArgs.Empty);
+        Close();
       }
     }
 
     private void _lblChar_MouseLeave(object sender, EventArgs e)
     {
-      ShowSecret = false;
+      if ((bool)Tag)
+        ShowSecret = false;
     }
 
     private void _lblChar_MouseEnter(object sender, EventArgs e)
     {
-      ShowSecret = (bool)Tag;
+      if ((bool)Tag)
+        ShowSecret = (bool)Tag;
     }
 
     private void Timer_Tick(object sender, EventArgs e)
     {
-      Font lblFont = Font;
-      if (lblFont.Size < 1)
+      _lblChar.Click -= TileDialog_Click;
+      fontSize--;
+      if (fontSize < 1)
       {
         timer.Enabled = false;
+        _lblChar.Click += TileDialog_Click;
         _lblChar.ForeColor = _lblChar.BackColor;
-        _lblChar.Font = new Font(_lblChar.Font.FontFamily,maxFontSize);
+        fontSize = maxFontSize;
+        _lblChar.Font = new Font(_lblChar.Font.FontFamily, fontSize, _lblChar.Font.Style);
         Invoke(_turnComplete);
+      }
+      else
+      {
+        _lblChar.Font = new Font(_lblChar.Font.FontFamily, fontSize, _lblChar.Font.Style);
       }
     }
 
     private void TileDialog_Click(object sender, EventArgs e)
     {
-      throw new NotImplementedException();
+      _guessClick?.Invoke(this, e);
+
     }
   }
 }

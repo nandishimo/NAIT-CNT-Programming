3105811c91ee27431b536f1569527ca7090bba7a
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2022-10-16 05:22:20 +0000

    copy of ICA05

---
+++ cmpe2300/ICA/Nandish-ICA06/Nandish-ICA05/Ball.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+namespace Nandish_ICA05
+{
+  internal class Ball:IComparable
+  {
+    private static CDrawer window = null;//static drawer member
+    private static Random rand = new Random();//static Random member
+    private float _radius;//float member for radius
+    public float Radius//set only property for radius
+    { set { _radius = Math.Abs(value); } }
+    private Color _color;//color member
+    private PointF _center;//position of ball center
+    public enum ESortType//enumeration for sorting
+    {
+      eRadius,
+      eDistance,
+      eColor
+    }
+    public ESortType _sort { get; set; }//automatic property of our enum type
+    
+    static Ball() //static contructor for creating drawer window with white background
+    {
+      window = new CDrawer();
+      window.BBColour = Color.White;
+      window.ContinuousUpdate = false;
+    }
+    public Ball(float radius)
+    { //instance contructor accepts radius, creates ball of speficifed size and random color and random position
+      Radius = radius;
+      _color = RandColor.GetColor();
+      _center = new PointF((float)(_radius + (window.ScaledWidth - 2 * _radius) * rand.NextDouble()),
+        (float)(_radius + (window.ScaledHeight - 2 * _radius) * rand.NextDouble()));
+    }
+    public static bool Loading
+    {//virtual property for clearing and rendering drawer
+      set
+      {
+        if (value) window.Clear();
+        else window.Render();
+      }
+    }
+    public static Point Location
+    {//manual property to set drawer window position
+      set
+      {
+        window.Position = value;
+      }
+    }
+    public void ShowBall()
+    {//method to add ball to drawer
+      window.AddCenteredEllipse((int)_center.X, (int)_center.Y, (int)_radius * 2, (int)_radius * 2, _color);
+    }
+    public float DistanceFrom(PointF point)
+    {//method to calculate disance between two points
+      double dx = this._center.X - point.X;
+      double dy = this._center.Y - point.Y;
+      return (float)Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2));
+    }
+    public override bool Equals(object obj)
+    {//check if balls overlap
+      if(!(obj is Ball ball))
+        return false;
+      return (DistanceFrom(ball._center)-this._radius-ball._radius)<0;
+    }
+    public override int GetHashCode()
+    {
+      return 1;
+    }
+    public int CompareTo(object obj)
+    {//compare 
+      if(!(obj is Ball ball))
+      {
+        throw new ArgumentException($"Ball:CompareTo:{nameof(obj)} - Not a valid Ball");
+      }
+      int value;
+      if (_sort == ESortType.eRadius)
+      {
+        value = _radius.CompareTo(ball._radius);
+        if(value == 0)
+        {
+          value = _color.ToArgb().CompareTo(ball._color.ToArgb());
+        }
+      }
+      else if (_sort == ESortType.eDistance)
+      {
+        value = (int)DistanceFrom(new PointF(0, 0)).CompareTo(ball.DistanceFrom(new PointF(0, 0)));
+      }
+      else
+      {
+        value = _color.ToArgb().CompareTo(ball._color.ToArgb()); 
+        if(value == 0)
+        {
+          value = -1*_radius.CompareTo(ball._radius);
+        }
+      }
+      return value;
+    }
+  }
+}
---
+++ cmpe2300/ICA/Nandish-ICA06/Nandish-ICA05/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Nandish_ICA05
+{
+  public partial class Form1 : Form
+  { 
+    List<Ball> balls = new List<Ball>();
+    float radius = -50;
+    Thread thread = null;
+    public Form1()
+    {
+      InitializeComponent();
+      Text = "ICA06";
+      _lblSize.MouseClick += _lblSize_MouseClick;
+      MouseWheel += Form1_MouseWheel;
+      thread = new Thread(ShowBalls);
+      thread.IsBackground = true;
+      //thread.Start();
+      _lblSize.Text = $"{radius}px";
+      StartPosition = FormStartPosition.Manual;
+      
+      Ball.Location = new Point(Location.X + Width, Location.Y);
+    }
+
+    private void Form1_MouseWheel(object sender, MouseEventArgs e)
+    {
+      if(e.Delta > 0)
+      {
+        radius++;
+      }
+      else
+      {
+        radius--;
+      }
+      _lblSize.Text = $"{radius}px";
+    }
+
+    private void _lblSize_MouseClick(object sender, MouseEventArgs e)
+    {
+      
+      int fail = 0;
+      int success = 0;
+      if(e.Button == MouseButtons.Left)
+      {
+        do
+        {
+          Ball ball = new Ball(radius);
+          if (balls.Contains(ball))
+          { //check if ball overlaps with existing balls and add if it is distinct
+            fail++;
+          }
+          else
+          {
+            lock (balls)
+              balls.Add(ball);
+            success++;
+          }
+          _pBar.Value = fail;
+        } while (fail < 1000 && success < 50);
+        if (thread.IsAlive)
+        {
+          thread.Abort();
+          
+        }
+        Invoke(new ThreadStart(ShowBalls));
+
+      }
+      if(e.Button == MouseButtons.Right)
+      {
+        lock (balls)
+          balls.RemoveRange(0,balls.Count/2);
+      }
+    }
+    void ShowBalls()
+    {
+        Ball.Loading = true;
+        foreach(Ball ball in balls)
+        {
+          lock (balls)
+            ball.ShowBall();
+          Thread.Sleep(2);
+          Ball.Loading = false;
+        }
+    }
+
+    private void _rbClick(object sender, EventArgs e)
+    {
+      if (_rbRadius.Checked)
+      {
+        foreach(Ball b in balls)
+        {
+          b._sort = Ball.ESortType.eRadius;
+        }
+        lock (balls)
+          balls.Sort();
+
+      }
+      else if (_rbDistance.Checked)
+      {
+        foreach (Ball b in balls)
+        { 
+          b._sort = Ball.ESortType.eDistance;
+        }
+        lock (balls)
+          balls.Sort();
+      }
+      else
+      {
+        foreach (Ball b in balls)
+        {
+          b._sort = Ball.ESortType.eColor;
+
+        }
+        lock (balls)
+          balls.Sort();
+      }
+      Invoke(new ThreadStart(ShowBalls));
+
+    }
+  }
+}

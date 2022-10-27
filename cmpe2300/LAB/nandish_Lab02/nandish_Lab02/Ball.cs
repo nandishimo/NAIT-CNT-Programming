440b207d693dad94ae756fc6ad8b90afd02a796f
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-10-27 16:04:23 +0000

    added pseudo code for ball class

--- cmpe2300/LAB/nandish_Lab02/nandish_Lab02/Ball.cs
+++ cmpe2300/LAB/nandish_Lab02/nandish_Lab02/Ball.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace nandish_Lab02
 {
   internal class Ball
   {
+    //public static float for friction, default 0.991f
+
+    //static Random object initialized
+
+    //Vector2 member for ball center
+
+    //vector2 member for ball velocity
+
+    //public property for ball center get only
+
+    //public property for ball velocity get only
+
+    //property int Radius public get, private set
+
+    //property int Hits public get, private set
+
+    //property int totalhits public get, private set
+
+    //property for ball color public get, private set
   }
 }

364a62306417a9bc774ace77d5494147f323de50
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-10-27 14:18:59 +0000

    clarifying ball class stuff

--- cmpe2300/ICA/nandish_ICA08/nandish_ICA08/Ball.cs
+++ cmpe2300/ICA/nandish_ICA08/nandish_ICA08/Ball.cs
@@ -10,24 +10,26 @@ namespace nandish_ICA08
 {
   internal class Ball
   {
-    //static Random object
+    //static Random object, initialized
     private static Random rand { get; set; } = new Random();
 
     //PointF field to hold ball location
     private PointF bCenter;
 
-    //System.Windows.Vector
+    //System.Windows.Vector for velocity
     private System.Windows.Vector velocity;
 
     //int field for ball radius
     private int bRadius;
 
+    //automatic color property
     public Color _color { get; set; }
-    //constructor
-    public Ball(PointF point, Color color)
+    
+    //constructor, accepting point for ball location and ball color
+    public Ball(PointF ballCenter, Color ballColor)
     {
-      bCenter = point;
-      _color = color;
+      bCenter = ballCenter;
+      _color = ballColor;
       velocity = new System.Windows.Vector(rand.NextDouble() * 12 - 6,rand.NextDouble() * 12 - 6);
       bRadius = rand.Next(20,51);
     }
@@ -57,21 +59,21 @@ namespace nandish_ICA08
         bCenter = new PointF(bRadius, bCenter.Y);
       }
 
-      //check right
+      //check right bound
       if (bCenter.X + bRadius > drawer.ScaledWidth)
       {
         velocity.X *= -1;
         bCenter = new PointF(drawer.ScaledWidth-bRadius, bCenter.Y);
       }
 
-      //check up bound
+      //check top bound
       if (bCenter.Y - bRadius < 0)
       {
         velocity.Y *= -1;
         bCenter = new PointF(bCenter.X, bRadius);
       }
 
-      //check down
+      //check bottom bound
       if (bCenter.X + bRadius > drawer.ScaledWidth)
       {
         velocity.Y *= -1;
@@ -79,11 +81,11 @@ namespace nandish_ICA08
       }
     }
 
-    //show function to render balls with number
-    public void Show(CDrawer drawer, int num)
+    //show function to render balls with int num(index of ball in list)
+    public void Show(CDrawer drawer, int index)
     {
       drawer.AddCenteredEllipse((int)bCenter.X, (int)bCenter.Y, bRadius * 2, bRadius * 2, _color);
-      drawer.AddText(num.ToString(), 15, (int)bCenter.X - bRadius, (int)bCenter.Y - bRadius, bRadius * 2, bRadius * 2);
+      drawer.AddText(index.ToString(), 15, (int)bCenter.X - bRadius, (int)bCenter.Y - bRadius, bRadius * 2, bRadius * 2);
     }
   }
 }

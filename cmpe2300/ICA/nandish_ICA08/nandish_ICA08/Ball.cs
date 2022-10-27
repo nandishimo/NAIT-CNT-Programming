64a472247561d3d226bb6afc7bb1927451665bda
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2022-10-27 06:16:08 +0000

    some weird semblance of code and stuff happening

---
+++ cmpe2300/ICA/nandish_ICA08/nandish_ICA08/Ball.cs
@@ -0,0 +1,89 @@
+﻿using GDIDrawer;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nandish_ICA08
+{
+  internal class Ball
+  {
+    //static Random object
+    private static Random rand { get; set; } = new Random();
+
+    //PointF field to hold ball location
+    private PointF bCenter;
+
+    //System.Windows.Vector
+    private System.Windows.Vector velocity;
+
+    //int field for ball radius
+    private int bRadius;
+
+    public Color _color { get; set; }
+    //constructor
+    public Ball(PointF point, Color color)
+    {
+      bCenter = point;
+      _color = color;
+      velocity = new System.Windows.Vector(rand.NextDouble() * 12 - 6,rand.NextDouble() * 12 - 6);
+      bRadius = rand.Next(20,51);
+    }
+
+    //Equals compares distance between centers to sum of radius of balls
+    public override bool Equals(object obj)
+    {
+      if(!(obj is Ball ball))
+      {
+        throw new ArgumentException($"Ball:Equals - {nameof(obj)} is not a ball");
+      }
+      return bRadius + ball.bRadius > Math.Sqrt(Math.Pow(bCenter.X - ball.bCenter.X, 2) - Math.Pow(ball.bCenter.Y - ball.bCenter.Y, 2));
+    }
+    public override int GetHashCode()
+    {
+      return 1;
+    }
+
+    //move function to bounce balls
+    public void Move(CDrawer drawer)
+    {
+      bCenter = new PointF(bCenter.X+ (float)velocity.X, bCenter.Y+(float)velocity.Y);
+      //check left bound
+      if(bCenter.X - bRadius < 0)
+      {
+        velocity.X *= -1;
+        bCenter = new PointF(bRadius, bCenter.Y);
+      }
+
+      //check right
+      if (bCenter.X + bRadius > drawer.ScaledWidth)
+      {
+        velocity.X *= -1;
+        bCenter = new PointF(drawer.ScaledWidth-bRadius, bCenter.Y);
+      }
+
+      //check up bound
+      if (bCenter.Y - bRadius < 0)
+      {
+        velocity.Y *= -1;
+        bCenter = new PointF(bCenter.X, bRadius);
+      }
+
+      //check down
+      if (bCenter.X + bRadius > drawer.ScaledWidth)
+      {
+        velocity.Y *= -1;
+        bCenter = new PointF(bCenter.X, drawer.ScaledHeight-bRadius);
+      }
+    }
+
+    //show function to render balls with number
+    public void Show(CDrawer drawer, int num)
+    {
+      drawer.AddCenteredEllipse((int)bCenter.X, (int)bCenter.Y, bRadius * 2, bRadius * 2, _color);
+      drawer.AddText(num.ToString(), 15, (int)bCenter.X - bRadius, (int)bCenter.Y - bRadius, bRadius * 2, bRadius * 2);
+    }
+  }
+}
---
+++ cmpe2300/ICA/nandish_ICA08/nandish_ICA08/Form1.cs
@@ -0,0 +1,96 @@
+﻿using GDIDrawer;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nandish_ICA08
+{
+  public partial class Form1 : Form
+  {
+    List<Ball> redBalls = new List<Ball>();
+    List<Ball> greenBalls = new List<Ball>();
+    List<Ball> blueBalls = new List<Ball>();
+    CDrawer upperDrawer = null;
+    CDrawer lowerDrawer = null;
+    public Form1()
+    {
+      InitializeComponent();
+      Timer timer = new Timer();
+      timer.Interval = 50;
+      timer.Enabled = true;
+      
+      timer.Tick += Timer_Tick;
+      upperDrawer = new CDrawer();
+      lowerDrawer = new CDrawer();
+      upperDrawer.ContinuousUpdate = lowerDrawer.ContinuousUpdate = false;
+      Shown += Form1_Shown;
+      timer.Start();
+    }
+
+    private void Form1_Shown(object sender, EventArgs e)
+    {
+      StartPosition = FormStartPosition.Manual;
+      Location = new Point(0, 0);
+      upperDrawer.Position = new Point(Width, 0);
+      lowerDrawer.Position = new Point(Width, upperDrawer.ScaledHeight);
+      
+    }
+
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+      if(upperDrawer.GetLastMouseLeftClick(out Point leftClick))
+      {
+        Ball redball = new Ball(new PointF((float)leftClick.X, (float)leftClick.Y), RandColor.GetColor());
+        if (redBalls.IndexOf(redball) == -1)
+        {
+          redBalls.Add(redball);
+        }
+      }
+      if (upperDrawer.GetLastMouseRightClick(out Point rightClick))
+      {
+        Ball greenball = new Ball(new PointF((float)leftClick.X, (float)leftClick.Y), RandColor.GetColor());
+        if (!greenBalls.Contains(greenball))
+        {
+          greenBalls.Insert(0,greenball);
+        }
+      }
+      foreach (Ball ball in redBalls)
+      {
+        ball.Move(upperDrawer);
+      }
+      foreach (Ball ball in greenBalls)
+      {
+        ball.Move(upperDrawer);
+      }
+      foreach(Ball ball in blueBalls)
+      {
+        ball.Move(lowerDrawer);
+      }
+      List<Ball> tmp = new List<Ball>(redBalls.Intersect(greenBalls));
+      redBalls.RemoveAll(ball => tmp.Contains(ball));
+      greenBalls.RemoveAll(ball => tmp.Contains(ball));
+      blueBalls = new List<Ball>(redBalls.Union(blueBalls));
+      upperDrawer.Clear();
+      upperDrawer.AddText($"Red : {redBalls.Count} Green : {greenBalls.Count}",50,Color.Cyan);
+      for(int i=0;i<redBalls.Count&&i<greenBalls.Count&&i<blueBalls.Count;i++)
+      {
+        if (redBalls[i] != null)
+          redBalls[i].Show(upperDrawer, i);
+        if (greenBalls[i] != null)
+          greenBalls[i].Show(upperDrawer, i);
+        if (blueBalls[i] != null)
+          blueBalls[i].Show(lowerDrawer, i);
+      }
+      upperDrawer.Render();
+      lowerDrawer.Render();
+    }
+  }
+}

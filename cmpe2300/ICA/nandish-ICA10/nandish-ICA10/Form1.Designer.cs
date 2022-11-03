a5bc42344c2c01a9692caa97ec412eb1ea2f9f99
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-11-03 16:38:31 +0000

    filedrop functionality working. cell colorizing works

--- cmpe2300/ICA/nandish-ICA10/nandish-ICA10/Form1.Designer.cs
+++ cmpe2300/ICA/nandish-ICA10/nandish-ICA10/Form1.Designer.cs
@@ -32,9 +32,9 @@
       this._btn_Average = new System.Windows.Forms.Button();
       this._lbl_FileName = new System.Windows.Forms.Label();
       this._dgv = new System.Windows.Forms.DataGridView();
-      this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+      this._bSource = new System.Windows.Forms.BindingSource(this.components);
       ((System.ComponentModel.ISupportInitialize)(this._dgv)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this._bSource)).BeginInit();
       this.SuspendLayout();
       // 
       // _btn_Average
@@ -43,7 +43,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
       this._btn_Average.Location = new System.Drawing.Point(12, 42);
       this._btn_Average.Name = "_btn_Average";
-      this._btn_Average.Size = new System.Drawing.Size(150, 26);
+      this._btn_Average.Size = new System.Drawing.Size(228, 26);
       this._btn_Average.TabIndex = 0;
       this._btn_Average.Text = "button1";
       this._btn_Average.UseVisualStyleBackColor = true;
@@ -52,9 +52,9 @@
       // 
       this._lbl_FileName.AutoSize = true;
       this._lbl_FileName.Location = new System.Drawing.Point(12, 9);
-      this._lbl_FileName.MinimumSize = new System.Drawing.Size(150, 30);
+      this._lbl_FileName.MinimumSize = new System.Drawing.Size(228, 30);
       this._lbl_FileName.Name = "_lbl_FileName";
-      this._lbl_FileName.Size = new System.Drawing.Size(150, 30);
+      this._lbl_FileName.Size = new System.Drawing.Size(228, 30);
       this._lbl_FileName.TabIndex = 1;
       this._lbl_FileName.Text = "label1";
       this._lbl_FileName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -64,21 +64,21 @@
       this._dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
       this._dgv.Location = new System.Drawing.Point(12, 74);
       this._dgv.Name = "_dgv";
-      this._dgv.Size = new System.Drawing.Size(150, 364);
+      this._dgv.Size = new System.Drawing.Size(228, 364);
       this._dgv.TabIndex = 2;
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(175, 450);
+      this.ClientSize = new System.Drawing.Size(252, 450);
       this.Controls.Add(this._dgv);
       this.Controls.Add(this._lbl_FileName);
       this.Controls.Add(this._btn_Average);
       this.Name = "Form1";
       this.Text = "Form1";
       ((System.ComponentModel.ISupportInitialize)(this._dgv)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this._bSource)).EndInit();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -89,7 +89,7 @@
     private System.Windows.Forms.Button _btn_Average;
     private System.Windows.Forms.Label _lbl_FileName;
     private System.Windows.Forms.DataGridView _dgv;
-    private System.Windows.Forms.BindingSource bindingSource1;
+    private System.Windows.Forms.BindingSource _bSource;
   }
 }
 
--- cmpe2300/ICA/nandish-ICA10/nandish-ICA10/Form1.cs
+++ cmpe2300/ICA/nandish-ICA10/nandish-ICA10/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Diagnostics.Trace;
+using System.IO;
 
 namespace nandish_ICA10
 {
@@ -17,7 +19,89 @@ namespace nandish_ICA10
     public Form1()
     {
       InitializeComponent();
-      bindingSource1 = new BindingSource(_dgv.DataSource, _dgv.);
+      _dgv.DataSource = null;// _bSource.DataSource;
+      _lbl_FileName.AllowDrop = true;
+      _lbl_FileName.DragEnter += _lbl_FileName_DragEnter;
+      _lbl_FileName.DragDrop += _lbl_FileName_DragDrop;
+      _btn_Average.Click += _btn_Average_Click;
+      _dgv.ColumnHeaderMouseClick += _dgv_ColumnHeaderMouseClick;
+      _dgv.CellFormatting += _dgv_CellFormatting;
+    }
+
+    private void _lbl_FileName_DragEnter(object sender, DragEventArgs e)
+    {
+      Text = "DragEnter";
+      e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.All : DragDropEffects.None;  
+    }
+
+    private void _dgv_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+    {
+      if(e.ColumnIndex == 1)
+      {
+        if ((int)e.Value < avgFrequency)
+        {
+          e.CellStyle.BackColor = Color.LightSalmon;
+        }
+        else
+        {
+          e.CellStyle.BackColor = Color.LightGreen;
+        }
+      }
+    }
+
+    private void _dgv_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+    {
+      throw new NotImplementedException();
+    }
+
+    private void _btn_Average_Click(object sender, EventArgs e)
+    {
+      throw new NotImplementedException();
+    }
+
+    private void _lbl_FileName_DragDrop(object sender, DragEventArgs e)
+    {
+      //check if file has been dropped
+      if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+        return;
+      //get list of files
+      string[] files = (string[])e.Data.GetData(DataFormats.FileDrop,false);
+      //only continue if single file dropped
+      if (files.Length != 1)
+        return;
+      LoadFile(new FileInfo(files[0]));
+
+    }
+    private void ShowDictionary()
+    {
+      if(values.Count==0) return;
+      avgFrequency = (int)values.Average(x=>x.Value);
+      _btn_Average.Text = "Average : " + avgFrequency.ToString();
+      //_bSource.DataSource = values.ToList();
+      _dgv.DataSource = values.ToList();
+      _dgv.Columns[0].HeaderText = "Key";
+      _dgv.Columns[1].HeaderText = "Value";
+      _dgv.Columns[0].DefaultCellStyle.Format = "X2";
+    }
+    private void LoadFile(FileInfo file)
+    {
+      _lbl_FileName.Text = file.Name;
+      values.Clear();
+      byte[] contents = File.ReadAllBytes(file.ToString());
+      WriteLine(file.ToString());
+      foreach(byte b in contents)
+      {
+        if (values.ContainsKey(b))
+        {
+          values[b]++;
+        }
+        else
+        {
+          values.Add(b, 1);
+        }
+      }
+      ShowDictionary();
+
     }
   }
 }

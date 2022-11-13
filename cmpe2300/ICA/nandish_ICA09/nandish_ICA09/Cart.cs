56d6329233bc0930fba2d8541368be75e1b4e1c7
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-11-13 23:15:53 +0000

    Cart class complete

---
+++ cmpe2300/ICA/nandish_ICA09/nandish_ICA09/Cart.cs
@@ -0,0 +1,75 @@
+﻿using GDIDrawer;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nandish_ICA09
+{
+  internal class Cart
+  {
+    private static Random _rand = new Random();
+    //only access Random object through this method to ensure thread-safe retrievals
+    public static int Next(int min, int max)
+    {
+      lock (_rand)
+      {
+        return _rand.Next(min, max);
+      }
+    }
+    public int maxProducts { get; }//max products loaded into cart
+    public Stack<char> _products { get;  private set; }//holds all products in cart
+    public Color _color { get; private set;}//cart color
+    public bool Done //return true if stack is empty
+    {
+      get
+      {
+        bool value = false;
+        if (_products.Count == 0)
+          value = true;
+        return value;
+      }
+    }
+    
+    /// <summary>
+    /// pops next product off stack. returns 0 if no pop
+    /// </summary>
+    /// <returns></returns>
+    public char Process()
+    {
+      char value = (char)0;
+      if(_products.Count != 0)
+      {
+        value = _products.Pop();
+      }
+      return value;
+    }
+    public Cart(string stuff)
+    {
+      _products = new Stack<char>(stuff.ToArray());
+      _color = RandColor.GetColor();
+      maxProducts = _products.Count;
+    }
+    public override string ToString()
+    {
+      string value="";
+      foreach(char c in _products)
+      {
+        value += c;
+      }
+      return value;
+    }
+    public void ShowCart(CDrawer drawer, int row, int position, int rowHeight)
+    {
+      Rectangle cart = new Rectangle(position, row * rowHeight, maxProducts, rowHeight);
+      drawer.AddRectangle(cart,_color,1,Color.White);
+      if (position == 0)
+      {
+        drawer.AddText(ToString(), 5, cart, Color.Black);
+      }
+    }
+
+  }
+}
---
+++ cmpe2300/ICA/nandish_ICA09/nandish_ICA09/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace nandish_ICA09
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this._btn_Simulate = new System.Windows.Forms.Button();
+      this._lbl_Remaining = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // _btn_Simulate
+      // 
+      this._btn_Simulate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this._btn_Simulate.Location = new System.Drawing.Point(12, 12);
+      this._btn_Simulate.Name = "_btn_Simulate";
+      this._btn_Simulate.Size = new System.Drawing.Size(150, 50);
+      this._btn_Simulate.TabIndex = 0;
+      this._btn_Simulate.Text = "button1";
+      this._btn_Simulate.UseVisualStyleBackColor = true;
+      // 
+      // _lbl_Remaining
+      // 
+      this._lbl_Remaining.AutoSize = true;
+      this._lbl_Remaining.BackColor = System.Drawing.SystemColors.Info;
+      this._lbl_Remaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this._lbl_Remaining.Location = new System.Drawing.Point(168, 12);
+      this._lbl_Remaining.MinimumSize = new System.Drawing.Size(300, 50);
+      this._lbl_Remaining.Name = "_lbl_Remaining";
+      this._lbl_Remaining.Size = new System.Drawing.Size(300, 50);
+      this._lbl_Remaining.TabIndex = 1;
+      this._lbl_Remaining.Text = "label1";
+      this._lbl_Remaining.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(481, 74);
+      this.Controls.Add(this._lbl_Remaining);
+      this.Controls.Add(this._btn_Simulate);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.Button _btn_Simulate;
+        private System.Windows.Forms.Label _lbl_Remaining;
+    }
+}
+

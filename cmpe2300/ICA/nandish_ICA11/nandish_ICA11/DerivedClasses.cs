f3a91c646d1fe32ac5646b849def79ed65c9203e
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-11-15 18:57:53 +0000

    Created project with GDIDrawer reference and derived Random class. Started RectDrawer class

---
+++ cmpe2300/ICA/nandish_ICA11/nandish_ICA11/DerivedClasses.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+namespace nandish_ICA11
+{
+  internal class DrawerRandom : Random
+  {
+    private int maxSize;
+    public DrawerRandom(int max)
+    {
+      maxSize = max;
+    }
+    public Rectangle NextDrawerRect(CDrawer drawer)
+    {
+      if(drawer == null)
+      {
+        throw new ArgumentNullException($"{nameof(drawer)} is null");
+      }
+      int sizeX = Next(10, maxSize);
+      int sizeY = Next(10, maxSize);
+      return new Rectangle(Next(0,drawer.ScaledWidth-sizeX), Next(0,drawer.ScaledHeight-sizeY), sizeX, sizeY);
+    }
+  }
+
+  internal class RectDrawer : GDIDrawer.CDrawer
+  {
+    private DrawerRandom _drand = null;
+    public RectDrawer()
+    {
+      CDrawer drawer = new CDrawer(400,800);
+      _drand = new DrawerRandom((int)0.2 * Math.Max(drawer.ScaledWidth, drawer.ScaledHeight));
+    }
+  }
+}

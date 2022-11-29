a64af9703d8bb19208acd26d1221d67e4b561af9
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-11-29 18:22:28 +0000

    added more classes

--- cmpe2300/LAB/nandish_LAB03/nandish_LAB03/Shapes.cs
+++ cmpe2300/LAB/nandish_LAB03/nandish_LAB03/Shapes.cs
@@ -12,6 +12,7 @@ namespace nandish_LAB03
   {
     //render instance to supplied drawer
     void Render(CDrawer dr);
+
   }
   public interface IAnimate
   {
@@ -22,10 +23,10 @@ namespace nandish_LAB03
     protected PointF _position { get; set; }
     protected Color _fill { get; set; }
     protected int _radius { get; set; }
-    protected Shape(PointF Position, Color Fill, int Radius=5)
+    protected Shape(PointF Position, Color? Fill=null, int Radius=5)
     {
       _position = Position;
-      _fill = Fill;
+      _fill = Fill.HasValue ? Fill.Value : Color.LightGray;
       if(Radius < 1)
         throw new ArgumentOutOfRangeException(nameof(Radius), "Radius cannot be less than 1");
       _radius = Radius;
@@ -39,6 +40,47 @@ namespace nandish_LAB03
       if (Sides < 3)
         throw new ArgumentOutOfRangeException(nameof(Sides), "Need a minimum of 3 sides");
       _sides = Sides;
+    }
+  }
+  internal class Shadow : Polygon
+  {
+    private double _sizeIncrease { get; set; }
+    public Shadow(PointF p, Color c, int r, int Sides, double delta = 0.5) : base(p, c, r, Sides)
+    {
+      _sizeIncrease = delta;
+    }
+  }
+  internal abstract class AniGon:Polygon
+  {
+    double _sequence { get; set; }
+    double _delta { get; set; }
+    protected AniGon(PointF p, Color c, int r, int Sides, double sequence, double delta):base(p,c,r,Sides)
+    {
+      _sequence = sequence;
+      _delta = delta;
+    }
+  }
+  internal class Spinner:AniGon
+  {
+    public Spinner(PointF p, Color c, int r, int sides, double dAniIncrement = 0, double dAniValue = 0) : base(p,c,r,sides,dAniIncrement,dAniValue)
+    {
+
+    }
+  }
+  internal abstract class AniChild : AniGon
+  {
+    double _distance { get; set; }
+    protected AniChild(PointF p, Color c, int r, int Sides, double sequence, double delta, double distance):base(p,c,r,Sides,sequence,delta)
+    {
+      _distance = distance;
+    }
+  }
+  internal class Orbiter : AniChild
+  {
+    PointF _xyMultiply { get; set; }
+    public Orbiter(Color c, int r, int sides, Shape parent, double dDistToParent, PointF ratio, double dAniIncrement = 0, double dAniValue = 0):base()
+    {
+
     }
   }
 }

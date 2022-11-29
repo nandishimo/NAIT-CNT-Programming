3b5017ad5175de46a012f990bfc0a7ec959fde0d
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2022-11-29 17:12:04 +0000

    Create shapes.cs file to store interfaces and classes

---
+++ cmpe2300/LAB/nandish_LAB03/nandish_LAB03/Shapes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GDIDrawer;
+
+namespace nandish_LAB03
+{
+  public interface IRenderable
+  {
+
+  }
+  internal class Shape
+  {
+  }
+}

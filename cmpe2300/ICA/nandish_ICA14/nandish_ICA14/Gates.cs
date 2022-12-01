d2d4718cf58a6fa17198ea496d3d0bc9750117dd
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2022-12-01 05:49:35 +0000

    tested classes

--- cmpe2300/ICA/nandish_ICA14/nandish_ICA14/Gates.cs
+++ cmpe2300/ICA/nandish_ICA14/nandish_ICA14/Gates.cs
@@ -8,9 +8,12 @@ namespace nandish_ICA14
 {
   abstract class Gate
   {
-    protected bool _IN1 { get { return _IN1; } set { _IN1 = value; } }
-    protected bool _IN2 { get { return _IN1; } set { _IN2 = value; } }
-    protected bool _OUT { get { return _OUT; } set { _OUT = value; } }
+    protected bool _IN1;
+    public bool IN1 { get { return _IN1; } }
+    protected bool _IN2;
+    public bool IN2 { get { return _IN1; } }
+    protected bool _OUT;
+    public bool OUT { get { return _OUT; } }
     public void Set(int A, int B)
     {
       _IN1 = A != 0;
@@ -21,41 +24,42 @@ namespace nandish_ICA14
       _IN1 = A;
       _IN2 = B;
     }
-    protected abstract void Latch();
-    protected abstract string Name { get; }
+    public abstract void Latch();
+    public abstract string Name { get; }
 
   }
   class NandGate : Gate
   {
-    protected override void Latch()
+    public override void Latch()
     {
       _OUT = !(_IN1 && _IN2);
     }
-    protected override string Name { get { return "NAND"; } }
+    public override string Name { get { return "NAND"; } }
   }
   class OrGate : Gate
   {
-    protected override void Latch()
+    public override void Latch()
     {
       _OUT = _IN1 || _IN2;
     }
-    protected override string Name { get { return "OR"; } }
+    public override string Name { get { return "OR"; } }
   }
   class XorGate : Gate
   {
-    protected override void Latch()
+    public override void Latch()
     {
       _OUT = (_IN1 && !_IN2) || (_IN2 && !_IN1);
     }
-    protected override string Name { get { return "XOR"; } }
+    public override string Name { get { return "XOR"; } }
   }
   class AndGate : NandGate
   {
-    protected override void Latch()
+    public override void Latch()
     {
-      
+      base.Latch();
+      _OUT = !_OUT;
     }
-    protected override string Name { get { return "AND"; } }
+    public override string Name { get { return "AND"; } }
   }
 
 }
--- cmpe2300/ICA/nandish_ICA14/nandish_ICA14/Program.cs
+++ cmpe2300/ICA/nandish_ICA14/nandish_ICA14/Program.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace nandish_ICA14
 {
   internal class Program
   {
+
     static void Main(string[] args)
     {
+      /*
+      Gate[] _gates = { new NandGate(), new OrGate(), new XorGate(), new AndGate() };
+      for (int i=0; i<_gates.Length; i++)
+      {
+        Console.WriteLine(ToTable(_gates[i]));
+      }
+      */
+      Gate[] _gates = { new AndGate(), new OrGate(), new NandGate(), new XorGate(), new NandGate(),
+                           new OrGate(), new AndGate(), new XorGate(), new AndGate(), new OrGate() };
+
+
+
+
+    }
+    private static string ToTable(Gate g)
+    {
+      StringBuilder s = new StringBuilder();
+      s.AppendLine($"A\tB\t{g.Name}");
+      for(int i=0;i<2;i++)
+      {
+        for(int j=0; j<2; j++)
+        {
+          g.Set(i, j);
+          g.Latch();
+          s.AppendLine($"{i}\t{j}\t{g.OUT}");
+        }
+      }
+
+      return s.ToString();
     }
   }
 }

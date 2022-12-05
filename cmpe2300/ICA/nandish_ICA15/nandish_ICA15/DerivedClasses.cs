d6245bcbf9d11dd4e4f48247d71434a10188c01b
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2022-12-05 18:58:18 +0000

    classes done, main form under construction

---
+++ cmpe2300/ICA/nandish_ICA15/nandish_ICA15/DerivedClasses.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+namespace nandish_ICA15
+{
+  // DrawerRandom class derived from Random.
+  // Supplies rectangles of random size (max 20% of greatest(width or height))
+  // rectangles are positioned to be fully contained within provided CDrawer
+  internal class DrawerRandom : Random
+  {
+    private int maxSize;
+    public DrawerRandom(int max)
+    {
+      maxSize = max;
+    }
+    public Rectangle NextDrawerRect(CDrawer drawer)
+    {
+      if(drawer == null)
+      {
+        throw new ArgumentNullException($"{nameof(drawer)} is null");
+      }
+      int sizeX = Next(10, maxSize);
+      int sizeY = Next(10, maxSize);
+      return new Rectangle(Next(0,drawer.ScaledWidth-sizeX), Next(0,drawer.ScaledHeight-sizeY), sizeX, sizeY);
+    }
+  }
+
+  // RectDrawer CDrawer derived class
+  // Uses DrawerRandom class to draw 100 random rectangles in 400x800 CDrawer
+  internal class RectDrawer : GDIDrawer.CDrawer
+  {
+    private DrawerRandom _drand = null;
+    public RectDrawer() : base(400,800)
+    {
+      _drand = new DrawerRandom(Math.Max(ScaledWidth, ScaledHeight)/5);
+      BBColour = Color.White;
+      for(int i=0;i<100;i++)
+        AddRectangle(_drand.NextDrawerRect(this), RandColor.GetKnownColor());
+    }
+  }
+
+  //PosDrawer class derived from CDrawer
+  //Creates a drawer whose position can be specified relative to another drawer or form
+  public class PosDrawer : GDIDrawer.CDrawer
+  {
+    public enum EPosition
+    {
+      eRight,
+      eBelow,
+      eBelowRight,
+      eNone
+    };
+    public void SetPosition (Form1 form, EPosition position)
+    {
+      int x;
+      int y;
+      if(position == EPosition.eRight)
+      {
+        x = form.Location.X + form.Width;
+        y = form.Location.Y;
+      }
+      else if(position == EPosition.eBelow)
+      {
+        x=form.Location.X;
+        y=form.Location.Y+form.Height;
+      }
+      else if(position == EPosition.eBelowRight)
+      {
+        x = form.Location.X + form.Width;
+        y = form.Location.Y + form.Height;
+      }
+      else
+      {
+        return;
+      }
+      this.Position = new Point(x, y);
+
+    }
+    public void SetPosition (CDrawer drawer, EPosition position)
+    {
+      int x;
+      int y;
+      if (position == EPosition.eRight)
+      {
+        x = drawer.Position.X + drawer.ScaledWidth;
+        y = drawer.Position.Y;
+      }
+      else if (position == EPosition.eBelow)
+      {
+        x = drawer.Position.X;
+        y = drawer.Position.Y + drawer.ScaledHeight;
+      }
+      else if (position == EPosition.eBelowRight)
+      {
+        x = drawer.Position.X + drawer.ScaledWidth;
+        y = drawer.Position.Y + drawer.ScaledHeight;
+      }
+      else
+      {
+        return;
+      }
+      this.Position = new Point(x, y);
+    }
+
+    public PosDrawer(int width=600, int height=400, Form1 form=null, EPosition position=EPosition.eNone) : base(width, height, false)
+    {
+      BBColour = Color.LemonChiffon;
+      if (form == null)
+        return;
+      SetPosition(form, position);
+      Render();
+      form.Activate();
+    }
+
+  }
+
+  // Derived from Posdrawer, can specify position of drawer relative to form
+  // Creates a appropriately sized drawer with half grayscale background image
+  public class PicDrawer : PosDrawer
+  {
+    public PicDrawer(Form1 form=null) : base(Properties.Resources.Yoshi.Width, Properties.Resources.Yoshi.Height, form, EPosition.eRight)
+    {
+      Bitmap pic = new Bitmap(Properties.Resources.Yoshi);
+      int picW = pic.Width;
+      int picH = pic.Height;
+
+      //iterate through each pixel of picture
+      for(int x = 0; x < picW; x++)
+      {
+        for(int y = 0; y < picH; y++)
+        {
+          Color color = pic.GetPixel(x, y);
+          if (x < picW / 2) //change pixels in left half of image to be grayscale
+          {
+            int R = color.R;
+            int G = color.G;
+            int B = color.B;
+            R = G = B = (R + G + B) / 3;
+            color = Color.FromArgb(R, G, B);
+          }
+          SetBBScaledPixel(x, y, color );
+        }
+      }
+      Render();
+    }
+  }
+}
---
+++ cmpe2300/ICA/nandish_ICA15/nandish_ICA15/Shapes.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+
+namespace nandish_ICA15
+{
+  public abstract class BaseShape
+  {
+    public static PicDrawer _canvas { get; set; } // Change to your PicDrawer
+    public static Random _rnd { get; private set; }
+    private Point _pt { get; set; } // Center of Shape
+    public Color _color { get; private set; } // Color of Shape
+    private double _dir = 0; // Radians vector of direction
+    private double _vel = 5; // Movement velocity
+    static BaseShape()
+    {
+      _rnd = new Random(0);
+    }
+    public BaseShape(Point pt, Color c)
+    {
+      _pt = pt;
+      _color = c;
+      // 0 is right, then counter-clockwise, in radians
+      _dir = _rnd.NextDouble() * 2 * Math.PI;
+    }
+    public Point Move() // NVI - public Move()
+    {
+      return VirtualMove(); // invokes VirtualMove()
+    }
+    public void Paint() // NVI - public Paint()
+    {
+      VirtualPaint(); // invokes VirtualPaint()
+    }
+    protected virtual Point VirtualMove()
+    {
+      // Adjust current direction by small random amount
+      _dir = _dir + (_rnd.NextDouble() * 0.8 - 0.4);
+      // Calculate new X, Y based on direction and velocity
+      int iNewX = _pt.X + (int)(_vel * Math.Cos(_dir));
+      int iNewY = _pt.Y - (int)(_vel * Math.Sin(_dir));
+      // Bounce by adding 90 degrees to direction
+      if (iNewX < 0 || iNewX >= _canvas.ScaledWidth)
+      {
+        _dir += Math.PI / 2;
+        iNewX = _pt.X;
+      }
+      if (iNewY < 0 || iNewY >= _canvas.ScaledHeight)
+      {
+        _dir += Math.PI / 2;
+        iNewY = _pt.Y;
+      }
+      _pt = new Point(iNewX, iNewY); // Save and return new Point
+      return _pt;
+    }
+    protected abstract void VirtualPaint();
+  }
+  interface IAnimatable
+  {
+    void Animate(CDrawer drawer);
+  }
+  interface IMortal
+  {
+    bool Step();
+  }
+  public class Snake : BaseShape, IMortal
+  {
+    private LinkedList<Point> _points;
+    private int Length;
+    private int Lives;
+    public Snake(Point point, int length) : base(point, Color.Red)
+    {
+      Length = length;
+      Lives = length * 10;
+    }
+    protected override Point VirtualMove()
+    {
+      Point pt = base.VirtualMove();
+      _points.AddFirst(pt);
+      if (_points.Count > Length)
+      {
+        _points.RemoveLast();
+      }
+      return pt;
+    }
+    protected override void VirtualPaint()
+    {
+      LinkedListNode<Point> pt = _points.First;
+      _canvas.AddCenteredEllipse(pt.Value, 3, 3, Color.Red);
+      for (int CountDown = Length; CountDown > 0; --CountDown)
+      {
+        if (pt.Next != null)
+          pt = pt.Next;
+        _canvas.AddCenteredEllipse(pt.Value, 3, 3, Color.FromArgb(255 * CountDown / Length, _color));
+        
+      }
+    }
+    public bool Step()
+    {
+      bool alive = true;
+      Lives--;
+      if (Lives <= 0)
+        alive = false;
+      return alive;
+    }
+  }
+  public class Blob : BaseShape, IAnimatable
+  {
+    private int Radius;
+    private Point LastLocation;
+    private double _Animate;
+    public Blob(Point pt, Color color, int radius):base(pt,color)
+    {
+      Radius = radius;
+      LastLocation = pt;
+    }
+    protected override Point VirtualMove()
+    {
+      Point pt =  base.VirtualMove();
+      LastLocation = pt;
+      return pt;
+    }
+    protected override void VirtualPaint()
+    {
+      int cRadius = Radius + (int)(0.5 * Radius * Math.Cos(_Animate));
+      _canvas.AddCenteredEllipse(LastLocation, cRadius, cRadius, _color);
+    }
+    public void Animate(CDrawer drawer)
+    {
+      _Animate += Math.PI/8;
+    }
+  }
+}

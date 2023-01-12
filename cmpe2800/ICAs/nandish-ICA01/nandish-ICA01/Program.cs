0e7d85e4774ff4ccd91edad3e63dfcf2815ede5a
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2023-01-12 21:47:05 +0000

    add test code and start of Categorize extension method

--- cmpe2800/ICAs/nandish-ICA01/nandish-ICA01/Program.cs
+++ cmpe2800/ICAs/nandish-ICA01/nandish-ICA01/Program.cs
@@ -10,6 +10,15 @@ namespace nandish_ICA01
   {
     static void Main(string[] args)
     {
+      List<int> iNums = new List<int>(new int[] { 4, 12, 4, 3, 5, 6, 7, 6, 12 });
+      foreach (KeyValuePair<int, int> scan in iNums.Categorize())
+        Console.WriteLine($"{scan.Key:d3} x {scan.Value:d5}");
+
+    }
+
+    public static Dictionary<int, int> Categorize(this List<int> sourcelist)
+    {
+
     }
   }
 }

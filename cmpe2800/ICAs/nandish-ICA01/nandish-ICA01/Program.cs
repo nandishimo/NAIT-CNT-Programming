da906651e435de07a531a780016f034850a8364d
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2023-01-13 23:33:58 +0000

    so far, so good

--- cmpe2800/ICAs/nandish-ICA01/nandish-ICA01/Program.cs
+++ cmpe2800/ICAs/nandish-ICA01/nandish-ICA01/Program.cs
@@ -17,21 +17,60 @@ namespace nandish_ICA01
   {
     static void Main(string[] args)
     {
+      Random _rnd = new Random();
+
       List<int> iNums = new List<int>(new int[] { 4, 12, 4, 3, 5, 6, 7, 6, 12 });
       foreach (KeyValuePair<int, int> scan in iNums.Categorize())
         Console.WriteLine($"{scan.Key:d3} x {scan.Value:d5}");
 
-    }
+      List<string> names = new List<string>(new string[] {
+      "Rick", "Glenn", "Rick", "Carl", "Michonne", "Rick", "Glenn" });
+      foreach (KeyValuePair<string, int> scan in names.Categorize())
+        Console.WriteLine($"{scan.Key} x {scan.Value:d5}");
 
+      LinkedList<char> llfloats = new LinkedList<char>();
+      while (llfloats.Count < 1000)
+        llfloats.AddLast((char)_rnd.Next('A', 'Z' + 1));
+      foreach (KeyValuePair<char, int> scan in llfloats.Categorize())
+        Console.WriteLine($"{scan.Key} x {scan.Value:d5}");
+      string TestString = "This is the test string, do not panic!";
+      foreach (KeyValuePair<char, int> scan in TestString.Categorize())
+        Console.WriteLine($"{scan.Key} x {scan.Value:d5}");
+    }
 
   }
 
   internal static class ExtentionMethods
   {
-    public static Dictionary<int, int> Categorize(this List<int> sourcelist)
+    static Random rand = new Random();
+    public static Dictionary<T, int> Categorize<T>(this IEnumerable<T> sourcelist)
     {
-      Dictionary<int, int> result = new Dictionary<int, int>();
+      Dictionary<T, int> result = new Dictionary<T, int>();
+      foreach (T i in sourcelist)
+      {
+        if (result.ContainsKey(i))
+        {
+          result[i]++;
+        }
+        else
+          result.Add(i, 1);
+      }
+      return result.OrderBy(kvp => kvp.Key).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
 
     }
+
+    public static T Rando<T>(this IEnumerable<T> source)
+    {
+      return source.ElementAt(rand.Next(0, source.Count()));
+    }
+
+    public static (key,val) Rando<key,val>(this Dictionary<key,val> source)
+    {
+      KeyValuePair<key, val> kvp = source.ElementAt(rand.Next(0, source.Count()));
+      return (kvp.Key, kvp.Value);
+    }
+
+
+
   }
 }

a29f81a3fc987dcab9031bb7f4d260f664ddca25
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2023-01-16 09:40:40 +0000

    toorderdlinkedlist done

--- cmpe2800/ICAs/nandish-ICA01/nandish-ICA01/Program.cs
+++ cmpe2800/ICAs/nandish-ICA01/nandish-ICA01/Program.cs
@@ -17,17 +17,31 @@ namespace nandish_ICA01
   {
     static void Main(string[] args)
     {
+      //create instance of Random
       Random _rnd = new Random();
 
+      //test code for Categorize on List<int>
+      Console.WriteLine("Test code for Categorize on ints");
+      Console.WriteLine("Original List: \n4, 12, 4, 3, 5, 6, 7, 6, 12 \n");
       List<int> iNums = new List<int>(new int[] { 4, 12, 4, 3, 5, 6, 7, 6, 12 });
+      Console.WriteLine("Categorized List:");
       foreach (KeyValuePair<int, int> scan in iNums.Categorize())
         Console.WriteLine($"{scan.Key:d3} x {scan.Value:d5}");
+      Console.WriteLine();
 
+      //test code for Categorize on List<string>
+      Console.WriteLine("Test code for Categorize on strings");
+      Console.WriteLine("Original List: \nRick, Glenn, Rick, Carl, Michonne, Rick, Glenn\n");
       List<string> names = new List<string>(new string[] {
       "Rick", "Glenn", "Rick", "Carl", "Michonne", "Rick", "Glenn" });
+      Console.WriteLine("Categorized List:");
       foreach (KeyValuePair<string, int> scan in names.Categorize())
         Console.WriteLine($"{scan.Key} x {scan.Value:d5}");
+      Console.WriteLine();
 
+      //test code for Categorize on LinkedList<char>
+      Console.WriteLine("Test code for Categorize on LinkedList<char>");
+      Console.WriteLine("Original List: \nRick, Glenn, Rick, Carl, Michonne, Rick, Glenn\n");
       LinkedList<char> llfloats = new LinkedList<char>();
       while (llfloats.Count < 1000)
         llfloats.AddLast((char)_rnd.Next('A', 'Z' + 1));
@@ -36,6 +50,36 @@ namespace nandish_ICA01
       string TestString = "This is the test string, do not panic!";
       foreach (KeyValuePair<char, int> scan in TestString.Categorize())
         Console.WriteLine($"{scan.Key} x {scan.Value:d5}");
+
+      List<int> testdataA = new List<int>(new int[] { 0, 1, 2, 3, 4, 5 });
+      for (int i = 0; i < 10; ++i)
+        Console.WriteLine($"Rando from list : {testdataA.Rando()}");
+      Dictionary<string, int> testdataB = new Dictionary<string, int>();
+      testdataB.Add("First", 1);
+      testdataB.Add("Second", 2);
+      testdataB.Add("Third", 3);
+      testdataB.Add("Forth", 4);
+      testdataB.Add("Fifth", 5);
+      for (int i = 0; i < 10; ++i)
+        Console.WriteLine($"Rando from dictionary : {testdataB.Rando()}");
+      Console.WriteLine($"AdjacentDuplicate with no duplicate :{ testdataA.AdjacentDuplicate()}");
+      testdataA.Add(5);
+            testdataA.Add(7);
+            Console.WriteLine($"AdjacentDuplicate with duplicate (5):{ testdataA.AdjacentDuplicate()}");
+      List<float> testdataC = new List<float>();
+      while (testdataC.Count < 10)
+        testdataC.Add((float)(_rnd.NextDouble() * 10));
+      foreach (float f in testdataC.ToOrderedLinkedList())
+        Console.WriteLine($"OrderedLL from List : {f}");
+
+      //CatStack<string> myStack = new CatStack<string>();
+      //myStack.Push("this");
+      //myStack.Push("is");
+      //myStack.Push("what");
+      //myStack.Push("this");
+      //myStack.Push("is");
+      //Console.WriteLine($"Cat element 1 : {myStack[1]}");
+      //Console.WriteLine($"Cat count 'is' : {myStack["is"]}");
     }
 
   }
@@ -66,7 +110,7 @@ namespace nandish_ICA01
       return source.ElementAt(rand.Next(0, source.Count()));
     }
 
-    public static (key,val) Rando<key,val>(this Dictionary<key,val> source)
+    public static (key, val) Rando<key, val>(this Dictionary<key, val> source)
     {
       if (source.Count() == 0)
         throw new ArgumentException($"{nameof(source)} is empty");
@@ -78,7 +122,7 @@ namespace nandish_ICA01
     {
       if (source.Count() == 0)
         throw new ArgumentException($"{nameof(source)} is empty");
-      for (int i = 0; i < source.Count()-1; i++)
+      for (int i = 0; i < source.Count() - 1; i++)
       {
         if (source.ElementAt(i).Equals(source.ElementAt(i++)))
         {
@@ -88,10 +132,10 @@ namespace nandish_ICA01
       return default(T);
     }
 
-    public static LinkedList<T> ToOrderedLinkedList<T>(this IEnumerable<IComparable<T>> source)
+    public static LinkedList<T> ToOrderedLinkedList<T>(this IEnumerable<T> source) where T : IComparable<T>
     {
       LinkedList<T> list = new LinkedList<T>();
-      foreach(T item in source)
+      foreach (T item in source)
       {
         if (list.Count == 0)
         {
@@ -99,12 +143,18 @@ namespace nandish_ICA01
         }
         else
         {
-
+          LinkedListNode<T> current = list.First;
+          while (current != null)
+          {
+            if (current.Value.CompareTo(item) < 0)
+            {
+              list.AddBefore(current, item); break;
+            }
+            current = current.Next;
+          }
         }
-
-
       }
-      
+      return list;
     }
 
   }

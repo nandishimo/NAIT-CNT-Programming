eaf0bfb7830e938a187665a16cb772de46580b0c
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2023-01-20 23:49:14 +0000

    started partB

---
+++ cmpe2800/ICAs/nandish-ICA02/nandish-ICA02/PartB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nandish_ICA02
+{
+  internal static  class PartB
+  {
+    static Random rand = new Random();
+    public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> sourceCollection)where T : IEnumerator
+    {
+      void Swap(T valA, T valB)
+      {
+        (valB, valA) = (valA, valB);
+      }
+      int n = sourceCollection.Count();
+      for (int i = 0; i < n; i++)
+      {
+        Swap(sourceCollection.ElementAt(i), sourceCollection.ElementAt(rand.Next(i, n)));
+      }
+      return sourceCollection;
+    }
+  }
+}

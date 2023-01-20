465be7eadcbe65fb763b2983696f1f7b24206bec
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2023-01-20 22:40:09 +0000

    Rejigged the CGrid class to generate only one value and return it. Adjusted how enumerator is mapped to grid around cursor to match sample code output

--- cmpe2800/ICAs/nandish-ICA02/nandish-ICA02/Program.cs
+++ cmpe2800/ICAs/nandish-ICA02/nandish-ICA02/Program.cs
@@ -10,6 +10,32 @@ namespace nandish_ICA02
   {
     static void Main(string[] args)
     {
+      {
+        Console.WriteLine("Corner test : \r\n");
+        CGrid myTestGrid = new CGrid(10, 8);
+        foreach ((int x, int y) q in myTestGrid)
+          if (q.x != -1 && q.y != -1)
+            Console.WriteLine($"Adjacent : {q.x}, {q.y}");
+        Console.WriteLine("\r\nCorner test done\r\n\r\n");
+      }
+      {
+        Console.WriteLine("Edge test : \r\n");
+        CGrid myTestGrid = new CGrid(10, 8);
+        myTestGrid.SetCursorPos(0, 4);
+        foreach ((int x, int y) q in myTestGrid)
+          if (q.x != -1 && q.y != -1)
+            Console.WriteLine($"Adjacent : {q.x}, {q.y}");
+        Console.WriteLine("\r\nEdge test done\r\n\r\n");
+      }
+      {
+        Console.WriteLine("Open test : \r\n");
+        CGrid myTestGrid = new CGrid(10, 8);
+        myTestGrid.SetCursorPos(3, 5);
+        foreach ((int x, int y) q in myTestGrid)
+          if (q.x != -1 && q.y != -1)
+            Console.WriteLine($"Adjacent : {q.x}, {q.y}");
+        Console.WriteLine("\r\nOpen test done\r\n\r\n");
+      }
     }
   }
 }

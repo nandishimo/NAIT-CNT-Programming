07169bdfb1c1073e7fc92989561fa0afa111432b
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2023-01-28 07:43:26 +0000

    first shot at PullMin

--- cmpe2800/ICAs/nandish-ICA02/nandish-ICA02/PartB.cs
+++ cmpe2800/ICAs/nandish-ICA02/nandish-ICA02/PartB.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,6 +26,19 @@ namespace nandish_ICA02
       {
         Swap(listCollection, i, rand.Next(i, n));
         yield return listCollection[i];
+      }
+    }
+
+    public static IEnumerable<T> PullMin<T>(this IEnumerable<T> sourceCollection) where T: IComparable
+    {
+      List<T> listCollection = new List<T>(sourceCollection);
+      HashSet<T> set = new HashSet<T>();
+      for (int i =0; i < listCollection.Count; i++)
+      {
+        var min = listCollection.Min();
+        yield return min;
+        set.Add(listCollection.Min());
+        listCollection.Remove(listCollection.Min());
       }
     }
   }

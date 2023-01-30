05081cd34719a80c78bf03a7cd1b2460314050f3
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2023-01-30 06:48:29 +0000

    created some classes

--- cmpe2800/Labs/nandish-LAB01/nandish-LAB01/Form1.cs
+++ cmpe2800/Labs/nandish-LAB01/nandish-LAB01/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GDIDrawer;
 
 namespace nandish_LAB01
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    const int _ciBlockSize = 10;
+    const int _ciBlocksX = 10;
+    const int _ciBlocksY = 10;
+    const int _ciWindowXSize = 500;
+    const int _ciWindowYSize = 500;
+    CDrawer _drawer = null;
+    GridManager _gridManager = null;
+
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
     }
+  }
 }
---
+++ cmpe2800/Labs/nandish-LAB01/nandish-LAB01/Grid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nandish_LAB01
+{
+  internal abstract class Grid
+  {
+
+  }
+  internal class GridRetainer : Grid
+  {
+
+  }
+  internal class Block : Grid
+  {
+
+  }
+}

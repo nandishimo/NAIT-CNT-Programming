361fdb1dff6d589931c532a520202d44e3e37b4b
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2023-02-01 13:43:24 +0000

    working on adding blocks and render/ticks

--- cmpe2800/Labs/nandish-LAB01/nandish-LAB01/Form1.cs
+++ cmpe2800/Labs/nandish-LAB01/nandish-LAB01/Form1.cs
@@ -14,7 +14,7 @@ namespace nandish_LAB01
 {
   public partial class Form1 : Form
   {
-    const int _ciBlockSize = 10;
+    const int _ciBlockSize = 50;
     const int _ciBlocksX = 10;
     const int _ciBlocksY = 10;
     const int _ciWindowXSize = 500;
@@ -22,9 +22,46 @@ namespace nandish_LAB01
     CDrawer _drawer = null;
     GridManager _gridManager = null;
 
+    Timer _ticker = new Timer();
+
     public Form1()
     {
       InitializeComponent();
+      _drawer = new CDrawer(_ciWindowXSize, _ciWindowYSize, false);
+      _gridManager = new GridManager(_ciWindowXSize, _ciWindowYSize);
+      KeyPreview = true;
+      _ticker.Tick += _ticker_Tick;
+      _drawer.MouseLeftClick += _drawer_MouseLeftClick;
+      _drawer.MouseRightClick += _drawer_MouseRightClick;
+    }
+
+    private void _drawer_MouseRightClick(Point pos, CDrawer dr)
+    {
+      //check if shift is pressed
+
+      //if so, try to add a solid block(non-falling)
+
+      //if not, try to kill a block in the clicked cell
+    }
+
+    private void _drawer_MouseLeftClick(Point pos, CDrawer dr)
+    {
+      //try to add a falling block in the clicked cell
+      throw new NotImplementedException();
+    }
+
+    private void _ticker_Tick(object sender, EventArgs e)
+    {
+      //tick the gridmanager's logic
+
+      //render blocks where the grtidmanager says they are and based on each block's properties/state
+
+
+    }
+    private void Render()
+    {
+      //_drawer.AddRectangle(block.Location.X, block.Location.Y, 50, 50);
+
     }
   }
 }

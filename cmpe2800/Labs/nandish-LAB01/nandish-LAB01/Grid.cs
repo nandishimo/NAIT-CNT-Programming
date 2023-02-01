849d1bf083bbaf77ab12f4fbcb90f2f538b64534
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2023-02-01 13:43:24 +0000

    working on adding blocks and render/ticks

--- cmpe2800/Labs/nandish-LAB01/nandish-LAB01/Grid.cs
+++ cmpe2800/Labs/nandish-LAB01/nandish-LAB01/Grid.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,31 @@ namespace nandish_LAB01
 {
   internal abstract class Grid
   {
+    protected Point _iiLocation;
+    protected Color _color;
+    public Point Location { get { return _iiLocation; } }
 
   }
   internal class GridRetainer : Grid
   {
-
+    public GridRetainer(int XPos, int YPos)
+    {
+      _iiLocation.X = XPos;
+      _iiLocation.Y = YPos;
+    }
   }
   internal class Block : Grid
   {
+    public Block(int XPos, int YPos, Color Color) 
+    {
+      _iiLocation.X = XPos;
+      _iiLocation.Y = YPos;
+      _color = Color;
+    }
+    public void Fall()
+    {
+      _iiLocation.Y--;
+    }
 
   }
 }
--- cmpe2800/Labs/nandish-LAB01/nandish-LAB01/GridManager.cs
+++ cmpe2800/Labs/nandish-LAB01/nandish-LAB01/GridManager.cs
@@ -1,13 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
 
 namespace nandish_LAB01
 {
   internal class GridManager
   {
+    private List<Grid> _grid = new List<Grid>();
+    private int _XSize;
+    private int _YSize;
+
+
+    public GridManager(int Width, int Height)
+    {
+      _XSize = Width;
+      _YSize = Height;
+    }
+
+
+    public void Render()
+    {
+      foreach (Block block in _grid)
+      {
+        
+      }
+    }
+
+    public bool AddFreeBlock(Point location)
+    {
+      if (_grid.Any(block => { return block.Location == location; }))
+      {
+        _grid.Add(new Block(location.X, location.Y, Color.Red));
+        return true;
+      }
+      else return false;
+
+    }
 
   }
 }

a58dd429e88760eccbd417b38bec62bc2a66b4e6
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2023-02-09 01:24:13 +0000

    Outputting to txt file works. Finding and displaying duplicates works

--- cmpe2800/ICAs/nandish-ICA03/nandish-ICA03/Form1.cs
+++ cmpe2800/ICAs/nandish-ICA03/nandish-ICA03/Form1.cs
@@ -22,18 +22,45 @@ namespace nandish_ICA03
       DragDrop += Form1_DragDrop;
     }
 
+    //handle a .txt file being dropped on the form
     private void Form1_DragDrop(object sender, DragEventArgs e)
     {
       var fname = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
       var sr = new StreamReader(fname);
       var contents = sr.ReadToEnd();
       sr.Close();
-      var sw = new StreamWriter("output.txt");
+      var sw = new StreamWriter("output.txt", false);
       var strings = contents.Split('\n').ToList();
-      strings.RemoveAll(s => s == " ");
+      for (int i = 0; i < strings.Count(); i++)
+      {
+        strings[i] = strings[i].Trim();
+      }
+      strings.RemoveAll(s => s == "");
+      var dict =  strings.Categorize();
+      foreach(var kvp in dict)
+      {
+        if(kvp.Value>1)
+          sw.WriteLine($"Duplicate found : {kvp.Key}");
+      }
+      /*
+      var newDict = new Dictionary<char,int>();
+      foreach(var str in strings)
+      {
+        newDict[str.First()]++;
+      }
+      newDict = newDict.OrderBy(kvp => kvp.Value).ToDictionary(kvp=>kvp.Key,kvp=>kvp.Value);
+      
+      foreach (var kvp in newDict)
+      {
+          //sw.WriteLine($"Words starting with {kvp.Key} : {kvp.Value}");
+      }
+
+      */
+      sw.Close();
 
     }
 
+    //change cursor when hovering over form while draggina an item
     private void Form1_DragEnter(object sender, DragEventArgs e)
     {
       if (e.Data.GetDataPresent(DataFormats.FileDrop))

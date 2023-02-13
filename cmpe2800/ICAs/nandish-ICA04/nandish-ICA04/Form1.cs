9fb229ae8716bca44513838025dbdeb37576f251
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2023-02-13 18:49:48 +0000

    created project

---
+++ cmpe2800/ICAs/nandish-ICA04/nandish-ICA04/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nandish_ICA04
+{
+	public partial class Form1 : Form
+	{
+		List<string> sourcestrings = new List<string>(new string[] { "Caballo", "Gato", "Perro", "Conejo", "Tortuga", "Cangrejo" });
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+	}
+}

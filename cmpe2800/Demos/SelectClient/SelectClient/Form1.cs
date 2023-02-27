2044b9409d2f09691c5afb2312e85bd29855bbac
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2023-02-27 18:50:38 +0000

    wow

---
+++ cmpe2800/Demos/SelectClient/SelectClient/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace SelectClient
+{
+	public partial class Form1 : Form
+	{
+		Socket sok = new Socket(
+			AddressFamily.InterNetwork, // IP V4 address scheme
+			SocketType.Stream, // streaming socket (connection based)
+			ProtocolType.Tcp); // use TCP as protocol
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void UI_btn_connect_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				sok.BeginConnect("www.microsoft.com", 80, Callback_ConnectDone, 42);
+			}
+			catch (Exception ex)
+			{
+
+				System.Diagnostics.Trace.WriteLine($"UI_btn_connect_Click - {ex.Message}");
+			}
+			
+		}
+
+		private void Callback_ConnectDone(IAsyncResult ar)
+		{
+			System.Diagnostics.Trace.WriteLine($"The thing I gave myself, {(int)ar.AsyncState}");
+			try
+			{
+				sok.EndConnect(ar);
+				System.Diagnostics.Trace.WriteLine("It worked!");
+			}
+			catch (Exception ex)
+			{
+
+				System.Diagnostics.Trace.WriteLine($"Callback_ConnectDone - {ex.Message}");
+			}
+			
+		}
+	}
+}

ef9e5b8cf9ffb3e5dbdb69c875f451100fb7b623
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2023-02-27 18:50:38 +0000

    wow

---
+++ cmpe2800/Demos/SelectClient/SelectClient/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SelectClient
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.UI_btn_connect = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// UI_btn_connect
+			// 
+			this.UI_btn_connect.Location = new System.Drawing.Point(389, 159);
+			this.UI_btn_connect.Name = "UI_btn_connect";
+			this.UI_btn_connect.Size = new System.Drawing.Size(75, 23);
+			this.UI_btn_connect.TabIndex = 0;
+			this.UI_btn_connect.Text = "button1";
+			this.UI_btn_connect.UseVisualStyleBackColor = true;
+			this.UI_btn_connect.Click += new System.EventHandler(this.UI_btn_connect_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.UI_btn_connect);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button UI_btn_connect;
+	}
+}
+

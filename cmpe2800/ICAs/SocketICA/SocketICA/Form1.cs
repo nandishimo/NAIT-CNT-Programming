e9f64379845742db0858dcf254c385012caff53a
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2023-03-06 18:23:34 +0000

    changed dialog project to class library

--- cmpe2800/ICAs/SocketICA/SocketICA/Form1.cs
+++ cmpe2800/ICAs/SocketICA/SocketICA/Form1.cs
@@ -12,6 +12,8 @@ namespace SocketICA
 {
 	public partial class ConnectDialog : Form
 	{
+		public string Address { get { return UI_tb_Address.Text; } }
+		public string Port { get { return UI_tb_Port.Text; } }
 		public ConnectDialog()
 		{
 			InitializeComponent();

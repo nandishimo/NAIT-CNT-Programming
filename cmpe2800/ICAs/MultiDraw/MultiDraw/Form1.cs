59e126962dfda3da338e36c32fb04afb575c10ce
1211_npatel101 <1211_npatel101@3acb9642-b6ae-9d4a-adef-73b665ade1aa>
2023-04-16 04:10:14 +0000

    not sure why socket isnt connecting

--- cmpe2800/ICAs/MultiDraw/MultiDraw/Form1.cs
+++ cmpe2800/ICAs/MultiDraw/MultiDraw/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing.Drawing2D;
-using JSONSuperSockect;
+//using JSONSuperSockect;
 using SocketICA;
 using Newtonsoft.Json;
 using static System.Diagnostics.Trace;
@@ -27,7 +27,68 @@ namespace MultiDraw
 			MouseMove += Form1_MouseMove;
 			MouseDown += Form1_MouseDown;
 		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			ConnectDialog connectDialog = new ConnectDialog();
+			if (connectDialog.ShowDialog() == DialogResult.OK)
+			{
+				_sock = new SuperSocket(connectDialog.Socket, StatusUpdate, DrawSegment);
+			}
+		}
 
+		private void Form1_MouseDown(object sender, MouseEventArgs e)
+		{
+			using (Graphics gr = CreateGraphics())
+			{
+				Brush pBrush = Brushes.Red;
+				gr.FillRectangle(pBrush, e.X, e.Y, 1, 1);
+			}
+		}
+
+		private void Form1_MouseMove(object sender, MouseEventArgs e)
+		{
+			Point mPosition = new Point(e.X, e.Y);
+			if (e.Button == MouseButtons.Left)
+			{
+				LineSegment seg = new LineSegment();
+				seg.SX = (Int16)lastPosition.X;
+				seg.SY = (Int16)lastPosition.Y;
+				seg.EX = (Int16)mPosition.X;
+				seg.EY = (Int16)mPosition.Y;
+				seg.C = Color.Red;
+				seg.T = 10;
+				_sock.SendData(JsonConvert.SerializeObject(seg));
+
+				using(Graphics gr = CreateGraphics())
+				{
+					//	Pen RedPen = new Pen(Color.Red, 10);
+					//	RedPen.SetLineCap(LineCap.Round, LineCap.Round, DashCap.Round);
+					//	gr.DrawLine(RedPen, lastPosition, mPosition);
+					//seg.Render(gr);
+				}
+					
+
+			}
+			lastPosition = mPosition;
+		}
+
+		private void Form1_Shown(object sender, EventArgs e)
+		{
+			/*
+			using (Graphics gr = CreateGraphics())
+			{
+				Pen BluePen = new Pen(Color.Blue);
+				for (int y = 0; y < ClientRectangle.Height; y += 10)
+				{
+					gr.DrawLine(BluePen, new Point(0, y), new Point(y, ClientRectangle.Height));
+				}
+				for (int y = 0; y < ClientRectangle.Height; y += 10)
+				{
+					gr.DrawLine(BluePen, new Point(y, 0), new Point(ClientRectangle.Height, y));
+				}
+			}
+			*/
+		}
 		private void StatusUpdate(string str)
 		{
 			try
@@ -45,160 +106,16 @@ namespace MultiDraw
 
 		private void DrawSegment(string json)
 		{
-			LineSegment seg = new LineSegment();
-		}
-
-		private void Form1_MouseDown(object sender, MouseEventArgs e)
-		{
-			using (Graphics gr = CreateGraphics())
-			{
-				Brush pBrush = Brushes.Red;
-				gr.FillRectangle(pBrush, e.X, e.Y, 1, 1);
-			}
-		}
-
-		private void Form1_MouseMove(object sender, MouseEventArgs e)
-		{
-			Point mPosition = new Point(e.X, e.Y);
-			if (e.Button == MouseButtons.Left)
+			LineSegment seg = LineSegment.JSONToLineSegment(json);
+			if(LineSegment.IsSane(seg))
 			{
 				using (Graphics gr = CreateGraphics())
 				{
-					//	Pen RedPen = new Pen(Color.Red, 10);
-					//	RedPen.SetLineCap(LineCap.Round, LineCap.Round, DashCap.Round);
-					//	gr.DrawLine(RedPen, lastPosition, mPosition);
-					LineSegment seg = new LineSegment();
-					seg.SX = (Int16)lastPosition.X;
-					seg.SY = (Int16)lastPosition.Y;
-					seg.EX = (Int16)mPosition.X;
-					seg.EY = (Int16)mPosition.X;
-					seg.C = Color.Red;
-					seg.T = 10;
 					seg.Render(gr);
-
-
 				}
-
-
-
 			}
-			lastPosition = mPosition;
+
 		}
 
-		private void Form1_Shown(object sender, EventArgs e)
-		{
-			
-			using (Graphics gr = CreateGraphics())
-			{
-				Pen BluePen = new Pen(Color.Blue);
-				/*
-				for(int i = 255; i>=0; --i)
-				{
-					BluePen.Color = Color.FromArgb(0, 0, i);
-					gr.DrawLine(BluePen, new Point(20 + i, 20), new Point(20 + i, 250));
-				}
-				*/
-				for (int y = 0; y < ClientRectangle.Height; y += 10)
-				{
-					gr.DrawLine(BluePen, new Point(0, y), new Point(y, ClientRectangle.Height));
-				}
-				for (int y = 0; y < ClientRectangle.Height; y += 10)
-				{
-					gr.DrawLine(BluePen, new Point(y, 0), new Point(ClientRectangle.Height, y));
-				}
-			}
-		}
-
-		private void Form1_Load(object sender, EventArgs e)
-		{
-			ConnectDialog connectDialog = new ConnectDialog();
-			if(connectDialog.ShowDialog() == DialogResult.OK)
-			{
-				_sock = new SuperSocket(connectDialog.Socket, StatusUpdate, DrawSegment);
-			}
-		}
-		// the JSON serialized version of this type must *not* contain any additional curly braces.
-		public class LineSegment
-		{
-			// start position
-			public Int16 SX { get; set; }
-			public Int16 SY { get; set; }
-			// end position
-			public Int16 EX { get; set; }
-			public Int16 EY { get; set; }
-			// colour
-			public Color C { get; set; }
-			// line thickness
-			public byte T { get; set; }
-
-			public void Render(Graphics gr)
-			{
-				using (Pen p = new Pen(C, T))
-				{
-					p.SetLineCap(
-					System.Drawing.Drawing2D.LineCap.Round,
-				   System.Drawing.Drawing2D.LineCap.Round,
-				   System.Drawing.Drawing2D.DashCap.Round);
-					gr.DrawLine(p, new PointF(SX, SY), new PointF(EX, EY));
-				}
-			}
-			// turn me (this) into a byte array via JSON
-			 public byte[] ToBytes()
-			{
-				try
-				{
-					string s = JsonConvert.SerializeObject(this);
-					//Console.WriteLine (s);
-					return Encoding.UTF8.GetBytes(s);
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine(e.Message);
-				}
-				return null;
-			}
-			// turn a JSON string into a LineSegment, with sanity test
-			public static LineSegment JSONToLineSegment(string json)
-			{
-				try
-				{
-					LineSegment seg = JsonConvert.DeserializeObject<LineSegment>(json);
-					if (seg != null)
-						if (IsSane(seg))
-							return seg;
-				}
-				catch (Exception err)
-				{
-					Console.WriteLine(err.Message);
-				}
-				return null;
-			}
-			// approximate sanity checking to ensure that what was
-			// JSON decoded has valid(ish) data
-			public static bool IsSane(LineSegment ls)
-			{
-				if (ls == null)
-					return false;
-				if (ls.SX > 10000)
-					return false;
-				if (ls.SY > 10000)
-					return false;
-				if (ls.EX > 10000)
-					return false;
-				if (ls.EY > 10000)
-					return false;
-				if (ls.SX < -10000)
-					return false;
-				if (ls.SY < -10000)
-					return false;
-				if (ls.EX < -10000)
-					return false;
-				if (ls.EY < -10000)
-					return false;
-				if (ls.T == 0)
-					return false;
-				return true;
-			}
-		}
 	}
 }

c8ef71d2e7c34c62f47b0d4121145115dd6e27db
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2023-04-17 16:20:47 +0000

    added a thing for no connection

--- cmpe2800/ICAs/SocketICA/SocketICA/Form1.cs
+++ cmpe2800/ICAs/SocketICA/SocketICA/Form1.cs
@@ -42,7 +42,17 @@ namespace SocketICA
 			{
 				WriteLine($"Error starting socket connection - {ex.Message}");
 			}
-			DialogResult = DialogResult.OK;
+			try
+			{
+				if(Socket.Connected)
+					DialogResult = DialogResult.OK;
+			}
+			catch (Exception)
+			{
+				WriteLine($"Error checking socket connection status");
+			}
+			Invoke(new Action(() => Text = $"Could not establish a connection"));
+			
 		}
 
 		private void UI_btn_Cancel_Click(object sender, EventArgs e)

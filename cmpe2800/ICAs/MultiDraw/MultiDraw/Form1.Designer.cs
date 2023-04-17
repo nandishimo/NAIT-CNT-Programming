738fae3cc00a83256907cd3d97796b763adc3866
Nandish Patel <97367672+nandishimo@users.noreply.github.com>
2023-04-17 16:52:00 +0000

    updated connection dialog

--- cmpe2800/ICAs/MultiDraw/MultiDraw/Form1.Designer.cs
+++ cmpe2800/ICAs/MultiDraw/MultiDraw/Form1.Designer.cs
@@ -28,21 +28,102 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.UI_btn_Connect = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// UI_btn_Connect
+			// 
+			this.UI_btn_Connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.UI_btn_Connect.Location = new System.Drawing.Point(12, 415);
+			this.UI_btn_Connect.Name = "UI_btn_Connect";
+			this.UI_btn_Connect.Size = new System.Drawing.Size(96, 26);
+			this.UI_btn_Connect.TabIndex = 0;
+			this.UI_btn_Connect.Text = "Connect";
+			this.UI_btn_Connect.UseVisualStyleBackColor = true;
+			this.UI_btn_Connect.Click += new System.EventHandler(this.UI_btn_Connect_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(114, 421);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(106, 20);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Thickness: 10";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(226, 421);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(152, 20);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Frames RXed: 0000";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(384, 421);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(121, 20);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Fragments: 000";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(511, 421);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(138, 20);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Destack Avg: 0.00";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(655, 421);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(133, 20);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "Bytes RXed: 0.00";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.UI_btn_Connect);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button UI_btn_Connect;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
 	}
 }
 
